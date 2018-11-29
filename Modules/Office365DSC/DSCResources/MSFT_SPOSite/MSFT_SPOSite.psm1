44bee320a66b3bbb74378e81b5ce4490ab755b8d
Nik Charlebois <nik_charlebois@hotmail.com>
2018-11-29 08:48:53 -0500

    Fix

--- Modules/Office365DSC/DSCResources/MSFT_SPOSite/MSFT_SPOSite.psm1
+++ Modules/Office365DSC/DSCResources/MSFT_SPOSite/MSFT_SPOSite.psm1
@@ -267,7 +267,7 @@ function Export-TargetResource
         [System.Management.Automation.PSCredential] 
         $GlobalAdminAccount
     )
-    Test-SPOServiceConnection -GlobalAdminAccount $GlobalAdminAccount
+    Test-SPOServiceConnection -GlobalAdminAccount $GlobalAdminAccount -SPOCentralAdminUrl $CentralAdminUrl
     $result = Get-TargetResource @PSBoundParameters
     $content = "SPOSite " + (New-GUID).ToString() + "`r`n"
     $content += "{`r`n"
