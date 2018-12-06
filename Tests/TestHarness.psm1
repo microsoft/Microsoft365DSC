ab7d460394e384e0f126fe2e7c90713c82e14fa0
Nik Charlebois <nik_charlebois@hotmail.com>
2018-12-06 08:30:02 -0500

    Updated TestHarness

--- Tests/TestHarness.psm1
+++ Tests/TestHarness.psm1
@@ -37,7 +37,8 @@ function Invoke-TestHarness
         $testResultSettings.Add('OutputFormat', 'NUnitXml' )
         $testResultSettings.Add('OutputFile', $TestResultsFile)
     }
-
+    Import-Module Microsoft.Online.SharePoint.PowerShell
+    Import-Module MSOnline
     Import-Module -Name "$repoDir\modules\Office365DSC\Office365DSC.psd1"
     $testsToRun = @()
 
