7c9d5adc2376be033ca1a93c872518ceed7149db
Nik Charlebois <nik_charlebois@hotmail.com>
2018-12-06 08:18:40 -0500

    Imported modules in test helper

--- Tests/Unit/UnitTestHelper.psm1
+++ Tests/Unit/UnitTestHelper.psm1
@@ -47,8 +47,8 @@ function New-O365DscUnitTestHelper
     Import-Module -Name $moduleToLoad -Global
 
     $initScript = @"
-            Remove-Module -Name "Microsoft.Online.SharePoint.PowerShell" -Force -ErrorAction SilentlyContinue
-            Remove-Module -Name "MSOnline" -Force -ErrorAction SilentlyContinue
+            Import-Module -Name "Microsoft.Online.SharePoint.PowerShell" -Force -ErrorAction SilentlyContinue
+            Import-Module -Name "MSOnline" -Force -ErrorAction SilentlyContinue
             Remove-Module -Name "AzureAD" -Force -ErrorAction SilentlyContinue
             Import-Module -Name "$StubModule" -WarningAction SilentlyContinue
             Import-Module -Name "$moduleToLoad"
