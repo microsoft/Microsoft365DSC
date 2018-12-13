3c33b6572ce024607273185b521492867802461c
Nik Charlebois <nik_charlebois@hotmail.com>
2018-12-13 10:54:17 -0500

    Updated

--- Modules/Office365DSC/Examples/ProofOfConcept/Example.ps1
+++ Modules/Office365DSC/Examples/ProofOfConcept/Example.ps1
@@ -1,5 +1,6 @@
 <#
 This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
 #>
 
 Configuration Example
