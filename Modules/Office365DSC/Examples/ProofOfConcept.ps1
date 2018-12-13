2a252def65b076a6481df3b13e1c58050cd49657
Nik Charlebois <nik_charlebois@hotmail.com>
2018-12-13 09:15:25 -0500

    Fixing Example

--- Modules/Office365DSC/Examples/ProofOfConcept.ps1
+++ Modules/Office365DSC/Examples/ProofOfConcept.ps1
@@ -1,10 +1,19 @@
 configuration ProofOfConcept
 {
-    Import-DSCResource -ModuleName Office365DSC -ModuleVersion 1.0.0.2
-    $credsGlobalAdmin = Get-Credential -UserName "TenantAdmin@O365DSC1.onmicrosoft.com" -Message "Global Admin"
+    param (
+        [Parameter(Mandatory=$true)] [ValidateNotNullorEmpty()] [PSCredential] $GlobalAdminAccount
+    )
+    Import-DSCResource -ModuleName Office365DSC
     Node localhost
     {
-        
+        EXOSharedMailbox AdminAssistants
+        {
+            DisplayName = "Test"
+            PrimarySMTPAddress = "Test@O365DSC1.onmicrosoft.com"
+            Aliases = @("Joufflu@o365dsc1.onmicrosoft.com", "Gilles@O365dsc1.onmicrosoft.com")
+            Ensure = "Present"
+            GlobalAdminAccount = $GlobalAdminAccount
+        }
 
         SPOSite HumanResources
         {
@@ -13,7 +22,7 @@ configuration ProofOfConcept
             StorageQuota = 300
             ResourceQuota = 500
             CentralAdminUrl = "https://o365dsc1-admin.sharepoint.com"
-            GlobalAdminAccount = $credsGlobalAdmin
+            GlobalAdminAccount = $GlobalAdminAccount
         }
     }
 }
