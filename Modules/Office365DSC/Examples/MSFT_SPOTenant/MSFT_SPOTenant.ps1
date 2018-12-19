8da98dce8a413481d4b954a3fb6d6f3d5d58cba6
Thorsten Loeschmann <thloesch@microsoft.com>
2018-12-19 16:43:35 +0100

    removed version numer when importing the dsc resource

--- Modules/Office365DSC/Examples/MSFT_SPOTenant/MSFT_SPOTenant.ps1
+++ Modules/Office365DSC/Examples/MSFT_SPOTenant/MSFT_SPOTenant.ps1
@@ -1,6 +1,6 @@
 Configuration MSFT_SPOTenant
 {
-    Import-DSCResource -ModuleName Office365DSC -ModuleVersion 1.0.0.2 
+    Import-DSCResource -ModuleName Office365DSC
     $credsGlobalAdmin = Get-Credential -UserName "TenantAdmin@O365DSC1.onmicrosoft.com" -Message "Global Admin"
     Node localhost
     {
