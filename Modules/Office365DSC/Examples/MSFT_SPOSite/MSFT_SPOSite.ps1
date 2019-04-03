f5005572eb22c01732612892053cd2cc3ec2d942
Thorsten Loeschmann <thloesch@microsoft.com>
2019-04-03 12:25:02 +0200

    SPOSite configuration

--- Modules/Office365DSC/Examples/MSFT_SPOSite/MSFT_SPOSite.ps1
+++ Modules/Office365DSC/Examples/MSFT_SPOSite/MSFT_SPOSite.ps1
@@ -38,6 +38,122 @@ Configuration MSFT_SPOSite
             DefaultSharingLinkType                      = "None"
             DefaultLinkPermission                       = "None"
         }
+        SPOSite ecd9c36d-d67b-46e6-801d-6faf145fd1d5
+        {
+            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite2"
+            CentralAdminUrl                             = "https://Office365DSC-admin.sharepoint.com"
+            StorageQuota                                = 26214400
+            LocaleId                                    = 1033
+            Template                                    = "STS#3"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
+            CompatibilityLevel                          = 15
+            Title                                       = "TestSite"
+            Ensure                                      = "Present"
+            DenyAddAndCustomizePages                    = $false
+            StorageQuotaWarningLevel                    = 25574400
+            LockState                                   = "Unlock"
+            SharingCapability                           = "Disabled"
+            CommentsOnSitePagesDisabled                 = $false
+            SocialBarOnSitePagesDisabled                = $false
+            DisableAppViews                             = "NotDisabled"
+            DisableCompanyWideSharingLinks              = "NotDisabled"
+            DisableFlows                                = "NotDisabled"
+            RestrictedToGeo                             = "BlockMoveOnly"
+            SharingDomainRestrictionMode                = "None"
+            SharingAllowedDomainList                    = ""
+            SharingBlockedDomainList                    = ""
+            ShowPeoplePickerSuggestionsForGuestUsers    = $false
+            DefaultSharingLinkType                      = "None"
+            DefaultLinkPermission                       = "None"
+        }
+        SPOSite 6603245d-2cf0-45fa-b2ce-46060b2ffaca
+        {
+            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite3"
+            CentralAdminUrl                             = "https://Office365DSC-admin.sharepoint.com"
+            StorageQuota                                = 26214400
+            LocaleId                                    = 1033
+            Template                                    = "STS#3"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
+            CompatibilityLevel                          = 15
+            Title                                       = "TestSite"
+            Ensure                                      = "Present"
+            DenyAddAndCustomizePages                    = $false
+            StorageQuotaWarningLevel                    = 25574400
+            LockState                                   = "Unlock"
+            SharingCapability                           = "ExternalUserSharingOnly"
+            CommentsOnSitePagesDisabled                 = $false
+            SocialBarOnSitePagesDisabled                = $false
+            DisableAppViews                             = "NotDisabled"
+            DisableCompanyWideSharingLinks              = "NotDisabled"
+            DisableFlows                                = "NotDisabled"
+            RestrictedToGeo                             = "BlockMoveOnly"
+            SharingDomainRestrictionMode                = "None"
+            SharingAllowedDomainList                    = ""
+            SharingBlockedDomainList                    = ""
+            ShowPeoplePickerSuggestionsForGuestUsers    = $false
+            DefaultSharingLinkType                      = "None"
+            DefaultLinkPermission                       = "None"
+        }
+        SPOSite ff3f784a-5611-4f61-81a4-4e17b1ee382b
+        {
+            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite4"
+            CentralAdminUrl                             = "https://Office365DSC-admin.sharepoint.com"
+            StorageQuota                                = 26214400
+            LocaleId                                    = 1033
+            Template                                    = "STS#3"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
+            CompatibilityLevel                          = 15
+            Title                                       = "TestSite"
+            Ensure                                      = "Present"
+            DenyAddAndCustomizePages                    = $false
+            StorageQuotaWarningLevel                    = 25574400
+            LockState                                   = "Unlock"
+            SharingCapability                           = "ExistingExternalUserSharingOnly"
+            CommentsOnSitePagesDisabled                 = $false
+            SocialBarOnSitePagesDisabled                = $false
+            DisableAppViews                             = "NotDisabled"
+            DisableCompanyWideSharingLinks              = "NotDisabled"
+            DisableFlows                                = "NotDisabled"
+            RestrictedToGeo                             = "BlockMoveOnly"
+            SharingDomainRestrictionMode                = "None"
+            SharingAllowedDomainList                    = ""
+            SharingBlockedDomainList                    = ""
+            ShowPeoplePickerSuggestionsForGuestUsers    = $false
+            DefaultSharingLinkType                      = "None"
+            DefaultLinkPermission                       = "None"
+        }
+        SPOSite dedbecc0-f869-4e8b-8518-f4d15e6257d7
+        {
+            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite5"
+            CentralAdminUrl                             = "https://Office365DSC-admin.sharepoint.com"
+            StorageQuota                                = 26214400
+            LocaleId                                    = 1033
+            Template                                    = "STS#3"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
+            CompatibilityLevel                          = 15
+            Title                                       = "TestSite"
+            Ensure                                      = "Present"
+            DenyAddAndCustomizePages                    = $false
+            StorageQuotaWarningLevel                    = 25574400
+            LockState                                   = "Unlock"
+            SharingCapability                           = "ExistingExternalUserSharingOnly"
+            CommentsOnSitePagesDisabled                 = $false
+            SocialBarOnSitePagesDisabled                = $false
+            DisableAppViews                             = "NotDisabled"
+            DisableCompanyWideSharingLinks              = "NotDisabled"
+            DisableFlows                                = "NotDisabled"
+            RestrictedToGeo                             = "BlockMoveOnly"
+            SharingDomainRestrictionMode                = "None"
+            SharingAllowedDomainList                    = ""
+            SharingBlockedDomainList                    = ""
+            ShowPeoplePickerSuggestionsForGuestUsers    = $false
+            DefaultSharingLinkType                      = "None"
+            DefaultLinkPermission                       = "None"
+        }
     }
 }
 
