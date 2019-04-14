da25cb767e876d52b2827c02f91fe53f7922e2c5
Nik Charlebois <nik_charlebois@hotmail.com>
2019-04-14 11:45:08 -0400

    Testing changes for valid examples to pass

--- Modules/Office365DSC/Examples/MSFT_EXOMailboxSettings/MSFT_EXOMailboxSettings.ps1
+++ Modules/Office365DSC/Examples/MSFT_EXOMailboxSettings/MSFT_EXOMailboxSettings.ps1
@@ -3,10 +3,15 @@ This example is used to test new resources and showcase the usage of new resourc
 It is not meant to use as a production baseline.
 #>
 
-Configuration MailboxSettings
+Configuration Example
 {
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsGlobalAdmin
+    )
     Import-DSCResource -ModuleName Office365DSC
-    $credsGlobalAdmin = Get-Credential -UserName "TenantAdmin@O365DSC1.onmicrosoft.com" -Message "Global Admin"
+
     Node localhost
     {
         EXOMailboxSettings OttawaTeam
@@ -19,15 +24,3 @@ Configuration MailboxSettings
         }
     }
 }
-
-$configData = @{
-    AllNodes = @(
-        @{
-            NodeName                    = "localhost"
-            PSDscAllowPlainTextPassword = $true;
-            PSDscAllowDomainUser        = $true;
-        }
-    )
-}
-
-MailboxSettings -ConfigurationData $configData
