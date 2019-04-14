a611d262b0e0bd728e3fbd0c4aa49420c2cb2157
Nik Charlebois <nik_charlebois@hotmail.com>
2019-04-14 14:59:18 -0400

    Final Changes

---
+++ Modules/Office365DSC/Examples/Resources/EXOMailboxSettings/1-ConfigureMailboxSettings.ps1
@@ -0,0 +1,26 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsGlobalAdmin
+    )
+    Import-DscResource -ModuleName Office365DSC
+
+    node localhost
+    {
+        EXOMailboxSettings OttawaTeam
+        {
+            DisplayName        = "Ottawa Employees"
+            TimeZone           = "Eastern Standard Time"
+            Locale             = "fr-CA"
+            Ensure             = "Present"
+            GlobalAdminAccount = $credsGlobalAdmin
+        }
+    }
+}
