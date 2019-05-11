4f96a85febba62eae36562956b31ac9a20ec1f90
Nik Charlebois <nicharl@microsoft.com>
2019-05-11 11:00:15 -0400

    Fixed O365User Tests

--- Modules/Office365DSC/DSCResources/MSFT_O365User/MSFT_O365User.psm1
+++ Modules/Office365DSC/DSCResources/MSFT_O365User/MSFT_O365User.psm1
@@ -453,7 +453,28 @@ function Test-TargetResource
     $CurrentValues = Get-TargetResource @PSBoundParameters
     $result = Test-Office365DSCParameterState -CurrentValues $CurrentValues `
                                            -DesiredValues $PSBoundParameters `
-                                           -ValuesToCheck @("Ensure")
+                                           -ValuesToCheck @("Ensure", `
+                                                            "UserPrincipalName", `
+                                                            "LicenseAssignment", `
+                                                            "UsageLocation", `
+                                                            "FirstName", `
+                                                            "LastName", `
+                                                            "DisplayName", `
+                                                            "City", `
+                                                            "Country", `
+                                                            "Department", `
+                                                            "Fax", `
+                                                            "MobilePhone", `
+                                                            "Office", `
+                                                            "PasswordNeverExpires", `
+                                                            "PhoneNumber", `
+                                                            "PostalCode", `
+                                                            "PreferredDataLocation", `
+                                                            "PreferredLanguage", `
+                                                            "State", `
+                                                            "StreetAddress", `
+                                                            "Title", `
+                                                            "UserType")
     Write-Verbose "Testing User $UserPrincipalName result was $result"
     return $result
 }
