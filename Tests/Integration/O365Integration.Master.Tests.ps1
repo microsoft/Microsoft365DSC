46bba502e239ffd5cc968339c9cd8b2b28731dbf
Nik Charlebois <nicharl@microsoft.com>
2019-07-03 16:51:44 -0400

    Update O365Integration.Master.Tests.ps1

--- Tests/Integration/O365Integration.Master.Tests.ps1
+++ Tests/Integration/O365Integration.Master.Tests.ps1
@@ -42,5 +42,5 @@ $ConfigurationData = @{
 
 $password = ConvertTo-SecureString $GlobalAdminPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($GlobalAdminUser, $password)
-DemoAlpha -ConfigurationData $ConfigurationData -GlobalAdmin $credential
+Master -ConfigurationData $ConfigurationData -GlobalAdmin $credential
 Start-DscConfiguration Master -Wait -Force -Verbose
