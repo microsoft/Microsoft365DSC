9d076a7696d417d74f27686ae7773af74707531f
Nik Charlebois <nik_charlebois@hotmail.com>
2019-10-03 09:58:48 -0400

    Update Office365DSC.SPOTheme.Tests.ps1

--- Tests/Unit/Office365DSC/Office365DSC.SPOTheme.Tests.ps1
+++ Tests/Unit/Office365DSC/Office365DSC.SPOTheme.Tests.ps1
@@ -129,7 +129,34 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
 
             Mock -CommandName Get-PnPTenantTheme -MockWith {
                 return @{
-                    Name = "TestTheme"
+                    Name               = "TestTheme"
+                    IsInverted         = $true
+                    Palette            = @{
+                        "themePrimary" = "#0078d4";
+                        "themeLighterAlt" = "#eff6fc";
+                        "themeLighter" = "#deecf9";
+                        "themeLight" = "#c7e0f4";
+                        "themeTertiary" = "#71afe5";
+                        "themeSecondary" = "#2b88d8";
+                        "themeDarkAlt" = "#106ebe";
+                        "themeDark" = "#005a9e";
+                        "themeDarker" = "#004578";
+                        "neutralLighterAlt" = "#f8f8f8";
+                        "neutralLighter" = "#f4f4f4";
+                        "neutralLight" = "#eaeaea";
+                        "neutralQuaternaryAlt" = "#dadada";
+                        "neutralQuaternary" = "#d0d0d0";
+                        "neutralTertiaryAlt" = "#c8c8c8";
+                        "neutralTertiary" = "#c2c2c2";
+                        "neutralSecondary" = "#858585";
+                        "neutralPrimaryAlt" = "#4b4b4b";
+                        "neutralPrimary" = "#333";
+                        "neutralDark" = "#272727";
+                        "black" = "#1d1d1d";
+                        "white" = "#fff";
+                        "bodyBackground" = "#0078d4";
+                        "bodyText" = "#fff";
+                    }
                 }
             }
 
