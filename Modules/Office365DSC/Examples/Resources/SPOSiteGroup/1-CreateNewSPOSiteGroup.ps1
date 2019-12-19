1800efa64c8db08259fab843829fe5f3c7665527
ThorstenLoeschmann <45271324+ThorstenLoeschmann@users.noreply.github.com>
2019-12-19 16:40:10 +0100

    Updated schema (2nd key) and sample

--- Modules/Office365DSC/Examples/Resources/SPOSiteGroup/1-CreateNewSPOSiteGroup.ps1
+++ Modules/Office365DSC/Examples/Resources/SPOSiteGroup/1-CreateNewSPOSiteGroup.ps1
@@ -17,7 +17,18 @@ Configuration Example
         SPOSiteGroup ee4a977d-4d7d-4968-9238-2a1702aa699c
         {
             Url                                         = "https://office365dsc.sharepoint.com/sites/testsite1"
-            Identity                                    = "TestSiteGroup"
+            Identity                                    = "TestSiteGroup1"
+            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
+            PermissionLevels                            = @("Edit", "Read")
+            Ensure                                      = "Present"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            
+        }
+
+        SPOSiteGroup adfd6217-29de-4297-95d4-7004455d3daa
+        {
+            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite1"
+            Identity                                    = "TestSiteGroup2"
             Owner                                       = "admin@Office365DSC.onmicrosoft.com"
             PermissionLevels                            = @("Edit", "Read")
             Ensure                                      = "Present"
