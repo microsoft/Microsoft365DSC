32b61ab2e544b57e73ab589543a9c1c2ab1c11d1
Mario (MSFT) <37979749+malauter@users.noreply.github.com>
2020-02-03 14:51:22 +0100

    Correct tests

--- Tests/Unit/Office365DSC/Office365DSC.EXOAvailabilityConfig.Tests.ps1
+++ Tests/Unit/Office365DSC/Office365DSC.EXOAvailabilityConfig.Tests.ps1
@@ -7,12 +7,15 @@ param(
             -Resolve)
 )
 
+$GenericStubPath = (Join-Path -Path $PSScriptRoot `
+        -ChildPath "..\Stubs\Generic.psm1" `
+        -Resolve)
 Import-Module -Name (Join-Path -Path $PSScriptRoot `
         -ChildPath "..\UnitTestHelper.psm1" `
         -Resolve)
 
 $Global:DscHelper = New-O365DscUnitTestHelper -StubModule $CmdletModule `
-    -DscResource "EXOAvailabilityConfig"
+    -DscResource "EXOAvailabilityConfig" -GenericStubModule $GenericStubPath
 Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
     InModuleScope -ModuleName $Global:DscHelper.ModuleName -ScriptBlock {
         Invoke-Command -ScriptBlock $Global:DscHelper.InitializeScript -NoNewScope
@@ -66,8 +69,8 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
                 Set-TargetResource @testParams
             }
 
-            It "Should return Present from the Get method" {
-                (Get-TargetResource @testParams).Ensure | Should Be "Present"
+            It "Should return Absent from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should Be "Absent"
             }
         }
 
