088180d79c9efee1dc0f8b483d4465582765d234
Nik Charlebois <nik_charlebois@hotmail.com>
2020-02-05 10:50:27 -0800

    Fixes for Unit Tests in SPOSite

--- Modules/Office365DSC/Examples/Resources/SPOSite/1-CreateNewSPOSite.ps1
+++ Modules/Office365DSC/Examples/Resources/SPOSite/1-CreateNewSPOSite.ps1
@@ -14,155 +14,27 @@ Configuration Example
 
     node localhost
     {
-        SPOSite ee4a977d-4d7d-4968-9238-2a1702aa699c
+        SPOSite DemoSite
         {
-            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite1"
+            Url                                         = "https://contoso.sharepoint.com/sites/testsite1"
             StorageQuota                                = 26214400
             LocaleId                                    = 1033
             Template                                    = "STS#3"
             GlobalAdminAccount                          = $credsGlobalAdmin
-            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
-            CompatibilityLevel                          = 15
+            Owner                                       = "admin@contoso.onmicrosoft.com"
             Title                                       = "TestSite"
             Ensure                                      = "Present"
-            DenyAddAndCustomizePages                    = $false
             StorageQuotaWarningLevel                    = 25574400
-            LockState                                   = "Unlock"
             SharingCapability                           = "Disabled"
             CommentsOnSitePagesDisabled                 = $false
-            SocialBarOnSitePagesDisabled                = $false
             DisableAppViews                             = "NotDisabled"
             DisableCompanyWideSharingLinks              = "NotDisabled"
-            DisableFlows                                = "NotDisabled"
-            RestrictedToGeo                             = "BlockMoveOnly"
+            DisableFlows                                = $false
             SharingDomainRestrictionMode                = "None"
             SharingAllowedDomainList                    = ""
             SharingBlockedDomainList                    = ""
-            ShowPeoplePickerSuggestionsForGuestUsers    = $false
             DefaultSharingLinkType                      = "None"
             DefaultLinkPermission                       = "None"
-            AnonymousLinkExpirationInDays               = "12"
-            OverrideTenantAnonymousLinkExpirationPolicy = $true
-        }
-        SPOSite ecd9c36d-d67b-46e6-801d-6faf145fd1d5
-        {
-            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite2"
-            StorageQuota                                = 26214400
-            LocaleId                                    = 1033
-            Template                                    = "STS#3"
-            GlobalAdminAccount                          = $credsGlobalAdmin
-            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
-            CompatibilityLevel                          = 15
-            Title                                       = "TestSite"
-            Ensure                                      = "Present"
-            DenyAddAndCustomizePages                    = $false
-            StorageQuotaWarningLevel                    = 25574400
-            LockState                                   = "Unlock"
-            SharingCapability                           = "Disabled"
-            CommentsOnSitePagesDisabled                 = $false
-            SocialBarOnSitePagesDisabled                = $false
-            DisableAppViews                             = "NotDisabled"
-            DisableCompanyWideSharingLinks              = "NotDisabled"
-            DisableFlows                                = "NotDisabled"
-            RestrictedToGeo                             = "BlockMoveOnly"
-            SharingDomainRestrictionMode                = "None"
-            SharingAllowedDomainList                    = ""
-            SharingBlockedDomainList                    = ""
-            ShowPeoplePickerSuggestionsForGuestUsers    = $false
-            DefaultSharingLinkType                      = "None"
-            DefaultLinkPermission                       = "None"
-            AnonymousLinkExpirationInDays               = "0"
-            OverrideTenantAnonymousLinkExpirationPolicy = $true
-        }
-        SPOSite 6603245d-2cf0-45fa-b2ce-46060b2ffaca
-        {
-            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite3"
-            StorageQuota                                = 26214400
-            LocaleId                                    = 1033
-            Template                                    = "STS#3"
-            GlobalAdminAccount                          = $credsGlobalAdmin
-            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
-            CompatibilityLevel                          = 15
-            Title                                       = "TestSite"
-            Ensure                                      = "Present"
-            DenyAddAndCustomizePages                    = $false
-            StorageQuotaWarningLevel                    = 25574400
-            LockState                                   = "Unlock"
-            SharingCapability                           = "ExternalUserSharingOnly"
-            CommentsOnSitePagesDisabled                 = $false
-            SocialBarOnSitePagesDisabled                = $false
-            DisableAppViews                             = "NotDisabled"
-            DisableCompanyWideSharingLinks              = "NotDisabled"
-            DisableFlows                                = "NotDisabled"
-            RestrictedToGeo                             = "BlockMoveOnly"
-            SharingDomainRestrictionMode                = "None"
-            SharingAllowedDomainList                    = ""
-            SharingBlockedDomainList                    = ""
-            ShowPeoplePickerSuggestionsForGuestUsers    = $false
-            DefaultSharingLinkType                      = "None"
-            DefaultLinkPermission                       = "None"
-            AnonymousLinkExpirationInDays               = "30"
-            OverrideTenantAnonymousLinkExpirationPolicy = $true
-        }
-        SPOSite ff3f784a-5611-4f61-81a4-4e17b1ee382b
-        {
-            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite4"
-            StorageQuota                                = 26214400
-            LocaleId                                    = 1033
-            Template                                    = "STS#3"
-            GlobalAdminAccount                          = $credsGlobalAdmin
-            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
-            CompatibilityLevel                          = 15
-            Title                                       = "TestSite"
-            Ensure                                      = "Present"
-            DenyAddAndCustomizePages                    = $false
-            StorageQuotaWarningLevel                    = 25574400
-            LockState                                   = "Unlock"
-            SharingCapability                           = "ExistingExternalUserSharingOnly"
-            CommentsOnSitePagesDisabled                 = $false
-            SocialBarOnSitePagesDisabled                = $false
-            DisableAppViews                             = "NotDisabled"
-            DisableCompanyWideSharingLinks              = "NotDisabled"
-            DisableFlows                                = "NotDisabled"
-            RestrictedToGeo                             = "BlockMoveOnly"
-            SharingDomainRestrictionMode                = "None"
-            SharingAllowedDomainList                    = ""
-            SharingBlockedDomainList                    = ""
-            ShowPeoplePickerSuggestionsForGuestUsers    = $false
-            DefaultSharingLinkType                      = "None"
-            DefaultLinkPermission                       = "None"
-            AnonymousLinkExpirationInDays               = "12"
-            OverrideTenantAnonymousLinkExpirationPolicy = $true
-        }
-        SPOSite dedbecc0-f869-4e8b-8518-f4d15e6257d7
-        {
-            Url                                         = "https://office365dsc.sharepoint.com/sites/testsite5"
-            StorageQuota                                = 26214400
-            LocaleId                                    = 1033
-            Template                                    = "STS#3"
-            GlobalAdminAccount                          = $credsGlobalAdmin
-            Owner                                       = "admin@Office365DSC.onmicrosoft.com"
-            CompatibilityLevel                          = 15
-            Title                                       = "TestSite"
-            Ensure                                      = "Present"
-            DenyAddAndCustomizePages                    = $false
-            StorageQuotaWarningLevel                    = 25574400
-            LockState                                   = "Unlock"
-            SharingCapability                           = "ExistingExternalUserSharingOnly"
-            CommentsOnSitePagesDisabled                 = $false
-            SocialBarOnSitePagesDisabled                = $false
-            DisableAppViews                             = "NotDisabled"
-            DisableCompanyWideSharingLinks              = "NotDisabled"
-            DisableFlows                                = "NotDisabled"
-            RestrictedToGeo                             = "BlockMoveOnly"
-            SharingDomainRestrictionMode                = "None"
-            SharingAllowedDomainList                    = ""
-            SharingBlockedDomainList                    = ""
-            ShowPeoplePickerSuggestionsForGuestUsers    = $false
-            DefaultSharingLinkType                      = "None"
-            DefaultLinkPermission                       = "None"
-            AnonymousLinkExpirationInDays               = "12"
-            OverrideTenantAnonymousLinkExpirationPolicy = $false
         }
     }
 }
