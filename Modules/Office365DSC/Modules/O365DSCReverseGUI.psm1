f05332021ecd475f13d77bae60228aef8c5b32db
Nik Charlebois <nik_charlebois@hotmail.com>
2020-04-07 14:24:34 -0400

    Update O365DSCReverseGUI.psm1

--- Modules/Office365DSC/Modules/O365DSCReverseGUI.psm1
+++ Modules/Office365DSC/Modules/O365DSCReverseGUI.psm1
@@ -350,6 +350,14 @@ function Show-O365GUI
             $chckBox.Left = 20
             $chckBox.Checked = $checked
             $chckBox.Top = $currentControlTop
+
+            # TODO: Remove once OneDrive is no longer an issue;
+            if ($resourceName -eq 'ODSettings')
+            {
+                $chckBox.Checked = $false;
+                $chckBox.Enabled = $false
+            }
+
             $ModeIdentifier.Top = $currentControlTop + 1
             $panel.Controls.Add($ModeIdentifier)
             $panel.Controls.Add($chckBox)
