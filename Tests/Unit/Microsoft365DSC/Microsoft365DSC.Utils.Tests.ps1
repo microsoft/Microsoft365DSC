0813ccf6909805aef9eb8e308ed0a186235d0b4a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-03 09:09:08 +0200

    Added tests for Remove-EmptySplatProperty

---
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.Utils.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Describe -Name 'Removing of empty / null splat properties' {
+    It 'From hashtable' {
+        function Test-FunctionHashtable
+        {
+            [cmdletBinding()]
+            param(
+                [ValidateNotNull()]$Test,
+                [string] $Test1
+            )
+            Write-Verbose "Value for Test is $Test, Value for Test1 is $Test1"
+        }
+
+        $Splat = @{
+            Test  = $NotExistingParameter
+            Test1 = 'Existing Entry'
+            Test2 = $null
+            Test3 = ''
+        }
+
+        Remove-EmptySplatProperty -Splat $Splat
+        { Test-FunctionHashtable @Splat } | Should -Not -Throw
+    }
+    It 'From OrderedDictionary' {
+        function Test-FunctionOrderedDictionary
+        {
+            [cmdletBinding()]
+            param(
+                [ValidateNotNull()]$Test,
+                [string] $Test1
+            )
+            Write-Verbose "Value for Test is $Test, Value for Test1 is $Test1"
+        }
+        $SplatDictionary = [ordered] @{
+            Test  = $NotExistingParameter
+            Test1 = 'Existing Entry'
+            Test2 = $null
+            Test3 = ''
+        }
+        Remove-EmptySplatProperty -Splat $SplatDictionary
+        { Test-FunctionOrderedDictionary @SplatDictionary } | Should -Not -Throw
+    }
+}
