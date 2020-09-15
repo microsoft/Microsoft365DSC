05247d8e0676dfea6512b8d1a2c204614ed594a1
timsto <34174209+Timsto@users.noreply.github.com>
2020-09-15 10:23:26 +0000

    Added MSFT_AADTenantDetails

---
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADTenantDetails/MSFT_AADTenantDetails.psm1
@@ -0,0 +1,229 @@
+function Get-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $MarketingNotificationEmails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationMails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationPhones,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $TechnicalNotificationMails,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $GlobalAdminAccount
+    )
+
+    Write-Verbose -Message "Getting configuration of AzureAD Tenant Details"
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName.Replace("MSFT_", "")
+    $data = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
+    $data.Add("Resource", $ResourceName)
+    $data.Add("Method", $MyInvocation.MyCommand)
+    $data.Add("Principal", $GlobalAdminAccount.UserName)
+    $data.Add("TenantId", $TenantId)
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+
+    $nullReturn = @{
+
+        $GlobalAdminAccount             = $GlobalAdminAccount
+    }
+
+    $ConnectionMode = New-M365DSCConnection -Platform 'AzureAD' -InboundParameters $PSBoundParameters
+
+    $CurrentParameters = $PSBoundParameters
+
+    try {
+        $AADTenantDetails = Get-AzureADTenantDetail
+    }
+    catch {
+        Write-Verbose -Message "Could not retriueve AzureAD Tenant Details"
+        return $nullReturn
+    }
+
+    $result = @{
+        TenantName                              = $AADTenantDetails.DisplayName
+        MarketingNotificationEmails             = $AADTenantDetails.MarketingNotificationEmails
+        SecurityComplianceNotificationMails     = $AADTenantDetails.SecurityComplianceNotificationMails
+        SecurityComplianceNotificationPhones    = $AADTenantDetails.SecurityComplianceNotificationPhones
+        TechnicalNotificationMails              = $AADTenantDetails.TechnicalNotificationMails
+    }
+    Write-Verbose -Message "Get-TargetResouce Result: `n $(Convert-M365DSCHashTabletoString -Hashtable $result)"
+    return $result
+}
+
+
+function Set-TargetResource {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $MarketingNotificationEmails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationMails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationPhones,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $TechnicalNotificationMails,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $GlobalAdminAccount
+    )
+
+    Write-Verbose -Message "Setting configuration of Azure AD Tenant Details"
+
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName.Replace("MSFT_", "")
+    $data = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
+    $data.Add("Resource", $ResourceName)
+    $data.Add("Method", $MyInvocation.MyCommand)
+    $data.Add("Principal", $GlobalAdminAccount.UserName)
+    #$data.Add("TenantId", $TenantId)
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+
+    $currentParameters = $PSBoundParameters
+    $currentParameters.Remove("GlobalAdminAccount") | Out-Null
+    try {
+        Set-AzureADTenantDetail @CurrentParameters
+    }
+    catch {
+        Write-Verbose -Message "Cannot Set AzureAD Tenant Details"
+    }
+}
+
+function Test-TargetResource  {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $MarketingNotificationEmails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationMails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationPhones,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $TechnicalNotificationMails,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $GlobalAdminAccount
+    )
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName.Replace("MSFT_", "")
+    $data = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
+    $data.Add("Resource", $ResourceName)
+    $data.Add("Method", $MyInvocation.MyCommand)
+    $data.Add("Principal", $GlobalAdminAccount.UserName)
+    $data.Add("TenantId", $TenantId)
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    Write-Verbose -Message "Testing configuration of AzureAD Tenant Details"
+
+    $CurrentValues = Get-TargetResouce @PSBoundParameters
+
+    Write-Verbose -Message "Target-Values: $(Convert-M365DscHashtableToString -Hashtable $PSBoundParameters)"
+    $ValuesToCheck = $PSBoundParameters
+    $ValuesToCheck.Remove('GlobalAdminAccount') | Out-Null
+
+    $TestResult = Test-Microsoft365DSCParameterState -CurrentValues $CurrentValues `
+        -Source $($MyInvocation.MyCommand.Source) `
+        -DesiredValues $PSBoundParameters `
+        -ValuesToCheck $ValuesToCheck.Keys
+
+    Write-Verbose -Message "Test-TargetResource returned $TestResult"
+
+    return $TestResult
+
+}
+
+function Export-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $MarketingNotificationEmails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationMails,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $SecurityComplianceNotificationPhones,
+
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $TechnicalNotificationMails,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $GlobalAdminAccount
+    )
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName.Replace("MSFT_", "")
+    $data = [System.Collections.Generic.Dictionary[[String], [String]]]::new()
+    $data.Add("Resource", $ResourceName)
+    $data.Add("Method", $MyInvocation.MyCommand)
+    $data.Add("Principal", $GlobalAdminAccount.UserName)
+    $data.Add("TenantId", $TenantId)
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    $dscContent = ''
+    $ConnectionMode = New-M365DSCConnection -Platform 'AzureAD' -InboundParameters $PSBoundParameters
+
+    $AADTenantDetails = Get-AzureADTenantDetail
+
+    $Params = @{
+        TenantName                              = $AADTenantDetails.DisplayName
+        MarketingNotificationEmails             = $AADTenantDetails.MarketingNotificationEmails
+        SecurityComplianceNotificationMails     = $AADTenantDetails.SecurityComplianceNotificationMails
+        SecurityComplianceNotificationPhones    = $AADTenantDetails.SecurityComplianceNotificationPhones
+        TechnicalNotificationMails              = $AADTenantDetails.TechnicalNotificationMails
+        GlobalAdminAccount                      = $GlobalAdminAccount
+        IsSingleIInstance                       = 'Yes'
+    }
+    $Results = Get-TargetResource @Params
+    $Results = Update-M365DSCExportAuthenticationResults -ConnectionMode $ConnectionMode -Results $Results
+    $dscContent += Get-M365DSCExportContentForResource -ResourceName $ResourceName -ConnectionMode $ConnectionMode `
+        -ModulePath $PSScriptRoot `
+        -Results $Results `
+        -GlobalAdminAccount $GlobalAdminAccount
+    Write-Host $Global:M365DSCEmojiGreenCheckMark
+    return $dscContent
+}
+
+Export-ModuleMember -function *-TargetResource
---
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADTenantDetails/readme.md
@@ -0,0 +1,16 @@
+# AAD Tenant Details
+
+## Description
+
+This resource configures the Azure AD Tenant Details
+
+## Azure AD Permissions
+
+To authenticate via Azure Active Directory, this resource required the following Application permissions:
+
+* **Automate**
+  * None
+* **Export**
+  * None
+
+NOTE: All permisions listed above require admin consent.
