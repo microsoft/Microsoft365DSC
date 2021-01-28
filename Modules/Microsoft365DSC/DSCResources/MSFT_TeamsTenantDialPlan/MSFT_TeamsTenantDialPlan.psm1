fe22507ab36574d2447419b969c1a1f134a05122
Nik Charlebois <nik.charlebois@microsoft.com>
2021-01-28 06:30:27 -0500

    Updates

--- Modules/Microsoft365DSC/DSCResources/MSFT_TeamsTenantDialPlan/MSFT_TeamsTenantDialPlan.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_TeamsTenantDialPlan/MSFT_TeamsTenantDialPlan.psm1
@@ -175,20 +175,41 @@ function Set-TargetResource
         # Ensure the VoiceNormalizationRules all exist
         foreach ($rule in $CurrentValues.NormalizationRules)
         {
-            $ruleName = $rule.Identity.Replace("Tag:", "")
-            $ruleObject = Get-CsVoiceNormalizationRule | Where-Object -FilterScript { $_.Name -eq $ruleName }
             if ($null -eq $ruleObject)
             {
                 # Need to create the rule
-                $ruleObject = New-CSVoiceNormalizationRule @rule
+                Write-Verbose "Creating VoiceNormalizationRule {$($rule.Identity)}"
+                $ruleObject = New-CSVoiceNormalizationRule -Identity "Global/$($rule.Identity.Replace('Tag:', ''))" `
+                    -Description $rule.Description `
+                    -Pattern $rule.Pattern `
+                    -Translation $rule.Translation `
+                    -InMemory
             }
-            $AllRules += $rueObject
+            $AllRules += $ruleObject
         }
     }
     #endregion
 
     if ($Ensure -eq 'Present' -and $CurrentValues.Ensure -eq 'Absent')
     {
+        #region VoiceNormalizationRules
+        $AllRules = @()
+        # Ensure the VoiceNormalizationRules all exist
+        foreach ($rule in $CurrentValues.NormalizationRules)
+        {
+            if ($null -eq $ruleObject)
+            {
+                # Need to create the rule
+                Write-Verbose "Creating VoiceNormalizationRule {$($rule.Identity)}"
+                $ruleObject = New-CSVoiceNormalizationRule -Identity "Global/$($rule.Identity.Replace('Tag:', ''))" `
+                    -Description $rule.Description `
+                    -Pattern $rule.Pattern `
+                    -Translation $rule.Translation `
+                    -InMemory
+            }
+            $AllRules += $ruleObject
+        }
+
         Write-Verbose -Message "Tenant Dial Plan {$Identity} doesn't exist. Creating it."
         $NewParameters = $PSBoundParameters
         $NewParameters.Remove("GlobalAdminAccount")
@@ -205,27 +226,74 @@ function Set-TargetResource
         $SetParameters.Remove("Ensure")
         $SetParameters.Remove("SimpleName")
 
-        $differences = Get-M365DSCNormalizationRulesDifference -Current $CurrentValues -Desired $PSBoundParameters
+        $desiredRules = @()
+        foreach ($rule in $NormalizationRules)
+        {
+            $desiredRule = @{
+                Identity            = $rule.Identity
+                Description         = $rule.Description
+                Pattern             = $rule.Pattern
+                IsExternalExtension = $rule.IsExternalExtension
+                Translation         = $rule.Translation
+            }
+            $desiredRules += $desiredRule
+        }
+
+        $differences = Get-M365DSCVoiceNormalizationRulesDifference -CurrentRules $CurrentValues.NormalizationRules -DesiredRules $desiredRules
 
         $rulesToRemove = @()
         $rulesToAdd = @()
 
-        foreach ($entry in $differences)
+        foreach ($ruleToAdd in $differences.RulesToAdd)
         {
-            $rule = Get-CsVoiceNormalizationRule | Where-Object -FilterScript { $_.Name -eq $entry.InputObject }
-            if ($entry.SideIndicator -eq '=>')
+            Write-Verbose "Adding new VoiceNormalizationRule {$($ruleToAdd.Identity)}"
+            $ruleObject = New-CSVoiceNormalizationRule -Identity "Global/$($ruleToAdd.Identity.Replace('Tag:', ''))" `
+                -Description $ruleToAdd.Description `
+                -Pattern $ruleToAdd.Pattern `
+                -Translation $ruleToAdd.Translation `
+                -InMemory
+            Write-Verbose "VoiceNormalizationRule created"
+            Set-CSTenantDialPlan -Identity $Identity -NormalizationRules @{Add = $ruleObject }
+            Write-Verbose "Updated the Tenant Dial Plan"
+        }
+        foreach ($ruleToRemove in $differences.RulesToRemove)
+        {
+            if ($null -eq $plan)
             {
-                $rulesToAdd += $rule
+                $plan = Get-CsTenantDialPlan -Identity $Identity
             }
-            elseif ($entry.SideIndicator -eq '<=')
+            $ruleObject = $plan.NormalizationRules | Where-Object -FilterScript { $_.Name -eq $ruleToRemove.Identity }
+
+            if ($null -ne $ruleObject)
             {
-                $rulesToRemove += $rule
+                Write-Verbose "Removing VoiceNormalizationRule {$($ruleToRemove.Identity)}"
+                Write-Verbose "VoiceNormalizationRule created"
+                Set-CSTenantDialPlan -Identity $Identity -NormalizationRules @{Remove = $ruleObject }
+                Write-Verbose "Updated the Tenant Dial Plan"
             }
         }
+        foreach ($ruleToUpdate in $differences.RulesToUpdate)
+        {
+            if ($null -eq $plan)
+            {
+                $plan = Get-CsTenantDialPlan -Identity $Identity
+            }
+            $ruleObject = $plan.NormalizationRules | Where-Object -FilterScript { $_.Name -eq $ruleToUpdate.Identity }
 
-        $SetParameters.NormalizationRules = @{Add = $rulesToAdd; Remove = $rulesToRemove }
-
-        Set-CSTenantDialPlan @SetParameters
+            if ($null -ne $ruleObject)
+            {
+                Write-Verbose "Updating VoiceNormalizationRule {$($ruleToUpdate.Identity)}"
+                Set-CSTenantDialPlan -Identity $Identity -NormalizationRules @{Remove = $ruleObject }
+                $ruleObject = New-CSVoiceNormalizationRule -Identity "Global/$($ruleToUpdate.Identity.Replace('Tag:', ''))" `
+                    -Description $ruleToUpdate.Description `
+                    -Pattern $ruleToUpdate.Pattern `
+                    -Translation $ruleToUpdate.Translation `
+                    -InMemory
+                Write-Verbose "VoiceNormalizationRule Updated"
+                Set-CSTenantDialPlan -Identity $Identity -NormalizationRules @{Add = $ruleObject }
+                Write-Verbose "Updated the Tenant Dial Plan"
+            }
+        }
     }
     elseif ($Ensure -eq 'Absent' -and $CurrentValues.Ensure -eq 'Present')
     {
@@ -292,16 +360,38 @@ function Test-TargetResource
     Write-Verbose -Message "Current Values: $(Convert-M365DscHashtableToString -Hashtable $CurrentValues)"
     Write-Verbose -Message "Target Values: $(Convert-M365DscHashtableToString -Hashtable $PSBoundParameters)"
 
-    $ValuesToCheck = $PSBoundParameters
     if ($null -ne $NormalizationRules)
     {
-        $differences = Get-M365DSCNormalizationRulesDifference -Current $CurrentValues -Desired $ValuesToCheck
-        if ($null -ne $differences)
+        $desiredRules = @()
+        foreach ($rule in $NormalizationRules)
+        {
+            $desiredRule = @{
+                Identity            = $rule.Identity
+                Description         = $rule.Description
+                Pattern             = $rule.Pattern
+                IsExternalExtension = $rule.IsExternalExtension
+                Translation         = $rule.Translation
+            }
+            $desiredRules += $desiredRule
+        }
+
+        if (-not $null -eq $CurrentValues.NormalizationRules)
+        {
+            $differences = Get-M365DSCVoiceNormalizationRulesDifference -CurrentRules $CurrentValues.NormalizationRules `
+                -DesiredRules $desiredRules
+        }
+        elseif ($NormalizationRules.Length -gt 0)
         {
             return $false
         }
     }
 
+    if ($differences.RulesToAdd.Length -gt 0 -or $differences.RulesToUpdate.Length -gt 0 -or $differences.RulesToRemove.Length -gt 0)
+    {
+        return $false
+    }
+
+    $ValuesToCheck = $PSBoundParameters
     $ValuesToCheck.Remove('GlobalAdminAccount') | Out-Null
     $ValuesToCheck.Remove("NormalizationRules") | Out-Null
 
@@ -393,6 +483,71 @@ function Export-TargetResource
     }
 }
 
+function Get-M365DSCVoiceNormalizationRulesDifference
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.Object[]]
+        $CurrentRules,
+
+        [Parameter(Mandatory = $true)]
+        [System.Object[]]
+        $DesiredRules
+    )
+
+    $differences = @{}
+    $rulesToRemove = @()
+    $rulesToAdd = @()
+    $rulesToUpdate = @()
+    foreach ($currentRule in $CurrentRules)
+    {
+        $equivalentDesiredRule = $DesiredRules | Where-Object -FilterScript { $_.Identity -eq $currentRule.Identity }
+
+        # Case the current rule is not listed in the Desired rules, we need to remove it
+        if ($null -eq $equivalentDesiredRule)
+        {
+            Write-Verbose "Adding Rule {$($currentRule.Identity)} to the RulesToRemove"
+            $rulesToRemove += $currentRule
+        }
+        # Case the rule exists but is not in the desired state
+        else
+        {
+            $differenceFound = $false
+            foreach ($key in $currentRule.Keys)
+            {
+                if (-not [System.String]::IsNullOrEmpty($equivalentDesiredRule.$key) -and $currentRule.$key -ne $equivalentDesiredRule.$key)
+                {
+                    $differenceFound = $true
+                }
+            }
+
+            if ($differenceFound)
+            {
+                Write-Verbose "Adding Rule {$($currentRule.Identity)} to the RulesToUpdate"
+                $rulesToUpdate += $equivalentDesiredRule
+            }
+        }
+    }
+
+    foreach ($desiredRule in $DesiredRules)
+    {
+        $equivalentCurrentRule = $CurrentRules | Where-Object -FilterScript { $_.Identity -eq $desiredRule.Identity }
+
+        # Case the desired rule doesn't exist, we need to create it
+        if ($null -eq $equivalentCurrentRule)
+        {
+            Write-Verbose "Adding Rule {$($desiredRule.Identity)} to the RulesToAdd"
+            $rulesToAdd += $desiredRule
+        }
+    }
+    $differences.Add("RulesToAdd", $rulesToAdd)
+    $differences.Add("RulesToUpdate", $rulesToUpdate)
+    $differences.Add("RulesToRemove", $rulesToRemove)
+    return $differences
+}
+
 function Get-M365DSCNormalizationRules
 {
     [CmdletBinding()]
@@ -408,18 +563,20 @@ function Get-M365DSCNormalizationRules
     }
 
     $result = @()
-    $allRules = Get-CsVoiceNormalizationRule
     foreach ($rule in $Rules)
     {
         $ruleName = $rule.Name.Replace("Tag:", "")
-        $ruleObject = $allRules | Where-Object -FilterScript { $_.Name -eq $ruleName }
         $currentRule = @{
             Identity            = $ruleName
-            Priority            = $ruleObject.Priority
-            Description         = $ruleObject.Description
-            Pattern             = $ruleObject.Pattern
-            Translation         = $ruleObject.Translation
-            IsInternalExtension = $ruleObject.IsInternalExtension
+            Priority            = $rule.Priority
+            Description         = $rule.Description
+            Pattern             = $rule.Pattern
+            Translation         = $rule.Translation
+            IsInternalExtension = $rule.IsInternalExtension
+        }
+        if ([System.String]::IsNullOrEmpty($rule.Priority))
+        {
+            $currentRule.Remove("Priority") | Out-Null
         }
         $result += $currentRule
     }
@@ -459,37 +616,6 @@ function Get-M365DSCNormalizationRulesAsString
     }
     $currentProperty += "            }"
     return $currentProperty
-}
-
-function Get-M365DSCNormalizationRulesDifference
-{
-    [CmdletBinding()]
-    [OutputType([System.Management.Automation.PSCustomObject])]
-    param (
-        [Parameter(Mandatory = $true)]
-        [System.Collections.Hashtable]
-        $Current,
-
-        [Parameter(Mandatory = $true)]
-        [System.Collections.Hashtable]
-        $Desired
-    )
-    $currentRulesId = @()
-    foreach ($currentRule in $CurrentValues.NormalizationRules)
-    {
-        Write-Verbose -Message "Adding {$($currentRule.Identity)} to current Rules"
-        $currentRulesId += $currentRule.Identity
-    }
-
-    $desiredRules = @()
-    foreach ($desiredRule in $NormalizationRules)
-    {
-        Write-Verbose -Message "Adding {$($desiredRule.Identity)} to desired Rules"
-        $desiredRules += $desiredRule.Identity
-    }
-
-    $differences = Compare-Object -ReferenceObject $currentRulesId -DifferenceObject $desiredRules
-    return $differences
 }
 
 Export-ModuleMember -Function *-TargetResource
