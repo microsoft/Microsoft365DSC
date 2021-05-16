325eb9ca0c6da69548fbb424210e9c1efe38f369
Ruud Gijsbers - Rademakers <40916740+RuudGijsbers@users.noreply.github.com>
2021-05-16 19:41:38 +0200

    updated Test TransportRul

--- Tests/Unit/Microsoft365DSC/Microsoft365DSC.EXOTransportRule.Tests.ps1
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.EXOTransportRule.Tests.ps1
@@ -15,7 +15,7 @@ Import-Module -Name (Join-Path -Path $M365DSCTestFolder `
         -Resolve)
 
 
-$Global:DscHelper = New-O365DscUnitTestHelper -StubModule $CmdletModule `
+$Global:DscHelper = New-M365DscUnitTestHelper -StubModule $CmdletModule `
     -DscResource "EXOTransportRule" -GenericStubModule $GenericStubPath
 Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
     InModuleScope -ModuleName $Global:DscHelper.ModuleName -ScriptBlock {
