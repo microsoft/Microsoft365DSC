7bf4dd5cd9778ddc756e2fd479ce7fd28c44cfb0
Sébastien Levert <slevert@outlook.com>
2021-07-26 16:46:51 -0400

    Adding contextual manifest + resources

--- generator/scripts/Build-Workloads.ps1
+++ generator/scripts/Build-Workloads.ps1
@@ -1,13 +1,15 @@
 param(
-    [String]$ResourcesOutputPath = "./generator/public/data/resources.json",
-    [String]$WorkloadsOutputPath = "./generator/public/data/workloads.json",
+    [String]$ResourcesOutputPath = "../public/data/resources.json",
+    [String]$WorkloadsOutputPath = "../public/data/workloads.json",
     [switch]$Force
 )
 
-$Workloads = Get-Content -Raw -Path "./generator/src/data/workloads.json" | ConvertFrom-Json
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+$Workloads = Get-Content -Raw -Path "../src/data/workloads.json" | ConvertFrom-Json
 $Resources = @()
 
-Get-ChildItem -Path "./Modules/Microsoft365DSC/DSCResources" -Directory | ForEach-Object {
+Get-ChildItem -Path "../../Modules/Microsoft365DSC/DSCResources" -Directory | ForEach-Object {
     $CurrentResource = $_.Name.Replace('MSFT_', '')
     $Workloads | ForEach-Object {
         $CurrentWorkload = $_
@@ -23,3 +25,5 @@ Get-ChildItem -Path "./Modules/Microsoft365DSC/DSCResources" -Directory | ForEac
 
 ConvertTo-Json -InputObject $Resources -Depth 5 | Out-File $ResourcesOutputPath -Force:$Force
 ConvertTo-Json -InputObject $Workloads -Depth 5 | Out-File $WorkloadsOutputPath -Force:$Force
+
+Pop-Location
