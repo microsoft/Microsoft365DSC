a1178f13cdc8f099e6ff2178aa79b6bbdbb89e97
Sébastien Levert <slevert@outlook.com>
2021-07-26 16:16:34 -0400

    Data Script updated

---
+++ generator/scripts/Build-Workloads.ps1
@@ -0,0 +1,28 @@
+param(
+    [String]$ResourcesOutputPath = "../public/data/resources.json",
+    [String]$WorkloadsOutputPath = "../public/data/workloads.json",
+    [switch]$Force
+)
+
+$Workloads = Get-Content -Raw -Path "../src/data/workloads.json" | ConvertFrom-Json
+Push-Location -Path ../../Modules/Microsoft365DSC/DSCResources
+$Resources = @()
+
+Get-ChildItem -Directory | ForEach-Object {
+    $CurrentResource = $_.Name.Replace('MSFT_', '')
+    $Workloads | ForEach-Object {
+        $CurrentWorkload = $_
+        if ($CurrentResource.StartsWith($CurrentWorkload.id))
+        {
+            $Resources += @{
+                "name"     = $CurrentResource;
+                "workload" = $_.id
+            }
+        }
+    }
+}
+
+Pop-Location
+
+ConvertTo-Json -InputObject $Resources -Depth 5 | Out-File $ResourcesOutputPath -Force:$Force
+ConvertTo-Json -InputObject $Workloads -Depth 5 | Out-File $WorkloadsOutputPath -Force:$Force
