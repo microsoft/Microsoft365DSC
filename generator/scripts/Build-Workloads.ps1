70e1ed5ddb20b49c59a5d8f5393d490e695b1053
Sébastien Levert <slevert@outlook.com>
2021-07-27 09:51:32 -0400

    Forcing folder creation

--- generator/scripts/Build-Workloads.ps1
+++ generator/scripts/Build-Workloads.ps1
@@ -1,14 +1,17 @@
 param(
-    [String]$ResourcesOutputPath = "../public/data/resources.json",
-    [String]$WorkloadsOutputPath = "../public/data/workloads.json",
+    [String]$DataPath = "../public/data",
+    [String]$ResourcesOutputPath = "$DataPath/resources.json",
+    [String]$WorkloadsOutputPath = "$DataPath/workloads.json",
     [switch]$Force
 )
 
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+# Getting the main workload file from the src data folder
 $Workloads = Get-Content -Raw -Path "../src/data/workloads.json" | ConvertFrom-Json
 $Resources = @()
 
+# For every resources found, generate an array of resources
 Get-ChildItem -Path "../../Modules/Microsoft365DSC/DSCResources" -Directory | ForEach-Object {
     $CurrentResource = $_.Name.Replace('MSFT_', '')
     $Workloads | ForEach-Object {
@@ -17,12 +20,19 @@ Get-ChildItem -Path "../../Modules/Microsoft365DSC/DSCResources" -Directory | Fo
         {
             $Resources += @{
                 "name"     = $CurrentResource;
-                "workload" = $_.id
+                "workload" = $CurrentWorkload.id
             }
         }
     }
 }
 
+# Creating the data folder if it doesn't exist
+if (!(Test-Path $DataPath))
+{
+    New-Item -ItemType Directory -Force -Path $DataPath | Out-Null
+}
+
+# Serializing on disk the resources and workloads files
 ConvertTo-Json -InputObject $Resources -Depth 5 | Out-File $ResourcesOutputPath -Force:$Force
 ConvertTo-Json -InputObject $Workloads -Depth 5 | Out-File $WorkloadsOutputPath -Force:$Force
 
