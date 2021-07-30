fa6926f547d759dba820d9371a43b826f28e8d41
Nik Charlebois <nik_charlebois@hotmail.com>
2021-07-30 15:34:18 -0400

    Update Generic.psm1

--- Tests/Unit/Stubs/Generic.psm1
+++ Tests/Unit/Stubs/Generic.psm1
@@ -198,7 +198,7 @@ function New-M365DSCConnection
                 "SecurityComplianceCenter", "MSOnline", "PnP", "PowerPlatforms", `
                 "MicrosoftTeams", "MicrosoftGraph")]
         [System.String]
-        $Platform,
+        $Workload,
 
         [Parameter(Mandatory = $true)]
         [System.Collections.Hashtable]
