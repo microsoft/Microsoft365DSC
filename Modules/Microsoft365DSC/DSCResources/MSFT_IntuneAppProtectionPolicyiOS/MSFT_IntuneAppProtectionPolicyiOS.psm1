9d888019670e33fc0f8c69da774aaa0dfb4721ba
Garima4496 <garima.das@nathcorp.com>
2021-08-24 13:52:09 +0530

    Merge branch 'F1_SPO' of https://github.com/Garima4496/Microsoft365DSC into F1_SPO

--- Modules/Microsoft365DSC/DSCResources/MSFT_IntuneAppProtectionPolicyiOS/MSFT_IntuneAppProtectionPolicyiOS.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_IntuneAppProtectionPolicyiOS/MSFT_IntuneAppProtectionPolicyiOS.psm1
@@ -673,10 +673,13 @@ function Export-TargetResource
     }
     catch
     {
-        Write-Host $Global:M365DSCEmojiRedX
         if ($_.Exception -like '*401*')
         {
             Write-Host "`r`n    $($Global:M365DSCEmojiYellowCircle) The current tenant is not registered for Intune."
+        }
+        else
+        {
+            Write-Host $Global:M365DSCEmojiRedX
         }
         try
         {
