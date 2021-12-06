2bc2985eca5f3bec6e962fb064b9da9ebc1e773d
nikcharlebois <nik_charlebois@hotmail.com>
2021-12-06 14:24:57 -0500

    Updated Unit Test action

--- Tests/TestHarness.psm1
+++ Tests/TestHarness.psm1
@@ -20,9 +20,6 @@ function Invoke-TestHarness
     Write-Verbose -Message 'Starting all Microsoft365DSC tests'
 
     $repoDir = Join-Path -Path $PSScriptRoot -ChildPath '..\' -Resolve
-    Import-Module Microsoft365DSC -Force
-    Update-M365DSCDependencies
-    Import-M365DSCDependencies
     $testCoverageFiles = @()
     if ($IgnoreCodeCoverage.IsPresent -eq $false)
     {
