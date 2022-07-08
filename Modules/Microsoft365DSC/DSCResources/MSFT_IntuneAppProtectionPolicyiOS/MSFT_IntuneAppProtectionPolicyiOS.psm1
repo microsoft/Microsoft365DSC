0c7c64922c833ca5091366c5bb23609896316f1e
Nik Charlebois <nicharl@microsoft.com>
2022-07-08 10:01:07 -0400

    Update MSFT_IntuneAppProtectionPolicyiOS.psm1

--- Modules/Microsoft365DSC/DSCResources/MSFT_IntuneAppProtectionPolicyiOS/MSFT_IntuneAppProtectionPolicyiOS.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_IntuneAppProtectionPolicyiOS/MSFT_IntuneAppProtectionPolicyiOS.psm1
@@ -951,11 +951,39 @@ function Get-M365DSCIntuneAppProtectionPolicyiOSJSON
         "simplePinBlocked": $($Parameters.SimplePinBlocked.ToString().ToLower()),
         "minimumPinLength": $($Parameters.MinimumPinLength),
         "managedBrowser": "$($Parameters.ManagedBrowser)",
+"@
+    if ($Parameters.MinimumWarningAppVersion)
+    {
+        $JsonContent += @"
         "minimumRequiredAppVersion": "$($Parameters.MinimumWarningAppVersion)",
+"@
+    }
+    if ($Parameters.MinimumRequiredOsVersion)
+    {
+        $JsonContent += @"
         "minimumRequiredOsVersion": "$($Parameters.MinimumRequiredOsVersion)",
+"@
+    }
+    if ($Parameters.MinimumRequiredSdkVersion)
+    {
+        $JsonContent += @"
         "minimumRequiredSdkVersion": "$($Parameters.MinimumRequiredSdkVersion)",
+"@
+    }
+    if ($Parameters.MinimumWarningAppVersion)
+    {
+        $JsonContent += @"
         "minimumWarningAppVersion": "$($Parameters.MinimumWarningAppVersion)",
+"@
+    }
+    if ($Parameters.MinimumWarningOsVersion)
+    {
+        $JsonContent += @"
         "minimumWarningOsVersion": "$($Parameters.MinimumWarningOsVersion)",
+"@
+    }
+
+    $JSONContent += @"
         "pinCharacterSet": "$($Parameters.PinCharacterSet)",
         "contactSyncBlocked": $($Parameters.ContactSyncBlocked.ToString().ToLower()),
         "periodBeforePinReset": "$($Parameters.PeriodBeforePinReset)",
