d4099e09371620218ddd775be2f4e34665a04a19
Sandro Lanfranchi <sandro.lanfranchi@swisscom.com>
2022-07-22 13:38:01 +0200

    wip: Add EnableSafeLinkforOffice / Email , DisableUrlRewrite parametrs

--- Modules/Microsoft365DSC/DSCResources/MSFT_EXOSafeLinksPolicy/MSFT_EXOSafeLinksPolicy.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_EXOSafeLinksPolicy/MSFT_EXOSafeLinksPolicy.psm1
@@ -46,6 +46,18 @@ function Get-TargetResource
         [Boolean]
         $EnableSafeLinksForTeams = $false,
 
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForEmail = $false,
+
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForOffice = $false,
+
+        [Parameter()]
+        [Boolean]
+        $DisableUrlRewrite = $false,
+
         #DEPRECATED
         [Parameter()]
         [Boolean]
@@ -153,6 +165,9 @@ function Get-TargetResource
                 EnableForInternalSenders      = $SafeLinksPolicy.EnableForInternalSenders
                 EnableOrganizationBranding    = $SafeLinksPolicy.EnableOrganizationBranding
                 EnableSafeLinksForTeams       = $SafeLinksPolicy.EnableSafeLinksForTeams
+                EnableSafeLinksForEmail       = $SafeLinksPolicy.EnableSafeLinksForEmail
+                EnableSafeLinksForOffice      = $SafeLinksPolicy.EnableSafeLinksForOffice
+                DisableUrlRewrite             = $SafeLinksPolicy.DisableUrlRewrite
                 #Deprecated
                 #IsEnabled                     = $SafeLinksPolicy.IsEnabled
                 ScanUrls                      = $SafeLinksPolicy.ScanUrls
@@ -243,6 +258,18 @@ function Set-TargetResource
         [Parameter()]
         [Boolean]
         $EnableSafeLinksForTeams = $false,
+
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForEmail = $false,
+
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForOffice = $false,
+
+        [Parameter()]
+        [Boolean]
+        $DisableUrlRewrite = $false,
 
         #DEPRECATED
         [Parameter()]
@@ -403,6 +430,18 @@ function Test-TargetResource
         [Parameter()]
         [Boolean]
         $EnableSafeLinksForTeams = $false,
+
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForEmail = $false,
+
+        [Parameter()]
+        [Boolean]
+        $EnableSafeLinksForOffice = $false,
+
+        [Parameter()]
+        [Boolean]
+        $DisableUrlRewrite = $false,
 
         #DEPRECATED
         [Parameter()]
