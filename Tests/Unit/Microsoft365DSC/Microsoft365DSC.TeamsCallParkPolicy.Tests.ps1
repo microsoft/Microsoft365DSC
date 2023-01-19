ce07d00509088b80352b926c0aa8d4d169a24cbb
Nik Charlebois <nicharl@microsoft.com>
2023-01-18 19:06:34 -0500

    TeamsCallParkPolicy - Initial Release

---
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.TeamsCallParkPolicy.Tests.ps1
@@ -0,0 +1,215 @@
+[CmdletBinding()]
+param(
+)
+$M365DSCTestFolder = Join-Path -Path $PSScriptRoot `
+                        -ChildPath "..\..\Unit" `
+                        -Resolve
+$CmdletModule = (Join-Path -Path $M365DSCTestFolder `
+            -ChildPath "\Stubs\Microsoft365.psm1" `
+            -Resolve)
+$GenericStubPath = (Join-Path -Path $M365DSCTestFolder `
+    -ChildPath "\Stubs\Generic.psm1" `
+    -Resolve)
+Import-Module -Name (Join-Path -Path $M365DSCTestFolder `
+        -ChildPath "\UnitTestHelper.psm1" `
+        -Resolve)
+
+$Global:DscHelper = New-M365DscUnitTestHelper -StubModule $CmdletModule `
+    -DscResource "TeamsCallParkPolicy" -GenericStubModule $GenericStubPath
+Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
+    InModuleScope -ModuleName $Global:DscHelper.ModuleName -ScriptBlock {
+        Invoke-Command -ScriptBlock $Global:DscHelper.InitializeScript -NoNewScope
+        BeforeAll {
+
+            $secpasswd = ConvertTo-SecureString "f@kepassword1" -AsPlainText -Force
+            $Credential = New-Object System.Management.Automation.PSCredential ("tenantadmin@mydomain.com", $secpasswd)
+
+            Mock -CommandName Confirm-M365DSCDependencies -MockWith {
+            }
+
+            Mock -CommandName Get-PSSession -MockWith {
+            }
+
+            Mock -CommandName Remove-PSSession -MockWith {
+            }
+
+            Mock -CommandName Set-CsTeamsCallParkPolicy -MockWith {
+            }
+
+            Mock -CommandName New-CsTeamsCallParkPolicy -MockWith {
+            }
+
+            Mock -CommandName Remove-CsTeamsCallParkPolicy -MockWith {
+            }
+
+            Mock -CommandName New-M365DSCConnection -MockWith {
+                return "Credential"
+            }
+        }
+        # Test contexts
+        Context -Name "The TeamsCallParkPolicy should exist but it DOES NOT" -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+                    Ensure                        = "Present"
+                    Credential                    = $Credential;
+                }
+
+                Mock -CommandName Get-CsTeamsCallParkPolicy -MockWith {
+                    return $null
+                }
+            }
+            It "Should return Values from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should -Be 'Absent'
+            }
+            It 'Should return false from the Test method' {
+                Test-TargetResource @testParams | Should -Be $false
+            }
+            It 'Should Create the group from the Set method' {
+                Set-TargetResource @testParams
+                Should -Invoke -CommandName New-CsTeamsCallParkPolicy -Exactly 1
+            }
+        }
+
+        Context -Name "The TeamsCallParkPolicy exists but it SHOULD NOT" -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+                    Ensure                        = "Absent"
+                    Credential                    = $Credential;
+                }
+
+                Mock -CommandName Get-CsTeamsCallParkPolicy -MockWith {
+                    return @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+
+                    }
+                }
+            }
+
+            It "Should return Values from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should -Be 'Present'
+            }
+
+            It 'Should return true from the Test method' {
+                Test-TargetResource @testParams | Should -Be $false
+            }
+
+            It 'Should Remove the group from the Set method' {
+                Set-TargetResource @testParams
+                Should -Invoke -CommandName Remove-CsTeamsCallParkPolicy -Exactly 1
+            }
+        }
+        Context -Name "The TeamsCallParkPolicy Exists and Values are already in the desired state" -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+                    Ensure                        = "Present"
+                    Credential                    = $Credential;
+                }
+
+                Mock -CommandName Get-CsTeamsCallParkPolicy -MockWith {
+                    return @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+
+                    }
+                }
+            }
+
+
+            It 'Should return true from the Test method' {
+                Test-TargetResource @testParams | Should -Be $true
+            }
+        }
+
+        Context -Name "The TeamsCallParkPolicy exists and values are NOT in the desired state" -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+                    Ensure                = "Present"
+                    Credential            = $Credential;
+                }
+
+                Mock -CommandName Get-CsTeamsCallParkPolicy -MockWith {
+                    return @{
+                    Description           = "FakeStringValueDrift #Drift"
+                    PickupRangeStart      = "FakeStringValueDrift #Drift"
+                    AllowCallPark         = $False
+                    ParkTimeoutSeconds    = $False
+                    PickupRangeEnd        = $False
+                    Identity              = "FakeStringValue"
+                    }
+                }
+            }
+
+            It "Should return Values from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should -Be 'Present'
+            }
+
+            It 'Should return false from the Test method' {
+                Test-TargetResource @testParams | Should -Be $false
+            }
+
+            It "Should call the Set method" {
+                Set-TargetResource @testParams
+                Should -Invoke -CommandName Set-CsTeamsCallParkPolicy -Exactly 1
+            }
+        }
+
+        Context -Name "ReverseDSC Tests" -Fixture {
+            BeforeAll {
+                $Global:CurrentModeIsExport = $true
+                $testParams = @{
+                    Credential = $Credential
+                }
+
+                Mock -CommandName Get-CsTeamsCallParkPolicy -MockWith {
+                    return @{
+                    Description           = "FakeStringValue"
+                    PickupRangeStart      = "FakeStringValue"
+                    AllowCallPark         = $True
+                    ParkTimeoutSeconds    = $True
+                    PickupRangeEnd        = $True
+                    Identity              = "FakeStringValue"
+
+                    }
+                }
+            }
+            It "Should Reverse Engineer resource from the Export method" {
+                Export-TargetResource @testParams
+            }
+        }
+    }
+}
+
+Invoke-Command -ScriptBlock $Global:DscHelper.CleanupScript -NoNewScope
