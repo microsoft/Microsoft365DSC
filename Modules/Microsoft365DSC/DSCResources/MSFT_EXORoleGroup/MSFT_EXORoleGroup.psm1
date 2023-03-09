b7480eba378d8abf3f92e3ef6b475c53f00eb688
Alex Floca <alexandru-ioan.floca@swisscom.com>
2023-03-09 06:34:36 -0800

    Initial release EXORoleGroup

---
+++ Modules/Microsoft365DSC/DSCResources/MSFT_EXORoleGroup/MSFT_EXORoleGroup.psm1
@@ -0,0 +1,460 @@
+function Get-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateLength(1, 64)]
+        [System.String]
+        $Name,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [System.String[]]
+        $Members,
+
+        [Parameter()]
+        [System.String[]]
+        $Roles,
+
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
+
+    Write-Verbose -Message "Getting Role Group configuration for $Name"
+    if ($Global:CurrentModeIsExport) {
+        $ConnectionMode = New-M365DSCConnection -Workload 'ExchangeOnline' `
+            -InboundParameters $PSBoundParameters `
+            -SkipModuleReload $true
+    }
+    else {
+        $ConnectionMode = New-M365DSCConnection -Workload 'ExchangeOnline' `
+            -InboundParameters $PSBoundParameters
+    }
+
+    #Ensure the proper dependencies are installed in the current environment.
+    Confirm-M365DSCDependencies
+
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName -replace 'MSFT_', ''
+    $CommandName = $MyInvocation.MyCommand
+    $data = Format-M365DSCTelemetryParameters -ResourceName $ResourceName `
+        -CommandName $CommandName `
+        -Parameters $PSBoundParameters
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    $nullReturn = $PSBoundParameters
+    $nullReturn.Ensure = 'Absent'
+
+    try {
+        $AllRoleGroups = Get-RoleGroup -ErrorAction Stop
+
+        $RoleGroup = $AllRoleGroups | Where-Object -FilterScript { $_.Name -eq $Name }
+
+        if ($null -eq $RoleGroup) {
+            Write-Verbose -Message "Role Group $($Name) does not exist."
+            return $nullReturn
+        }
+        else {
+            # Get RoleGroup Members DN if RoleGroup exists. This is required especially when adding Members like "Exchange Administrator" or "Global Administrator" that have different Names across Tenants
+            $roleGroupMember = Get-RoleGroupMember -Identity $Name | Select-Object DisplayName
+
+            $result = @{
+                Name                  = $RoleGroup.Name
+                Description           = $RoleGroup.Description
+                Members               = $roleGroupMember.DisplayName
+                Roles                 = $RoleGroup.Roles
+                Ensure                = 'Present'
+                Credential            = $Credential
+                ApplicationId         = $ApplicationId
+                CertificateThumbprint = $CertificateThumbprint
+                CertificatePath       = $CertificatePath
+                CertificatePassword   = $CertificatePassword
+                Managedidentity       = $ManagedIdentity.IsPresent
+                TenantId              = $TenantId
+            }
+
+            Write-Verbose -Message "Found Role Group $($Name)"
+            return $result
+        }
+    }
+    catch {
+        New-M365DSCLogEntry -Message 'Error retrieving data:' `
+            -Exception $_ `
+            -Source $($MyInvocation.MyCommand.Source) `
+            -TenantId $TenantId `
+            -Credential $Credential
+
+        return $nullReturn
+    }
+}
+
+function Set-TargetResource {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateLength(1, 64)]
+        [System.String]
+        $Name,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [System.String[]]
+        $Members,
+
+        [Parameter()]
+        [System.String[]]
+        $Roles,
+
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
+
+    Write-Verbose -Message "Setting Role Group configuration for $Name"
+
+    $currentRoleGroupConfig = Get-TargetResource @PSBoundParameters
+
+    #Ensure the proper dependencies are installed in the current environment.
+    Confirm-M365DSCDependencies
+
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName -replace 'MSFT_', ''
+    $CommandName = $MyInvocation.MyCommand
+    $data = Format-M365DSCTelemetryParameters -ResourceName $ResourceName `
+        -CommandName $CommandName `
+        -Parameters $PSBoundParameters
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    $ConnectionMode = New-M365DSCConnection -Workload 'ExchangeOnline' `
+        -InboundParameters $PSBoundParameters
+
+    $NewRoleGroupParams = @{
+        Name        = $Name
+        Description = $Description
+        Members     = $Members
+        Roles       = $Roles
+        Confirm     = $false
+    } 
+    # Remove Description Parameter if null or Empty as the creation fails with $null parameter
+    if ([System.String]::IsNullOrEmpty($Description)) {
+        $NewRoleGroupParams.Remove('Description') | Out-Null
+    }
+
+    # CASE: Role Group doesn't exist but should;
+    if ($Ensure -eq 'Present' -and $currentRoleGroupConfig.Ensure -eq 'Absent') {
+        Write-Verbose -Message "Role Group '$($Name)' does not exist but it should. Create and configure it."
+        # Create Role Group
+        New-RoleGroup @NewRoleGroupParams
+
+    }
+    # CASE: Role Group exists but it shouldn't;
+    elseif ($Ensure -eq 'Absent' -and $currentRoleGroupConfig.Ensure -eq 'Present') {
+        Write-Verbose -Message "Role Group '$($Name)' exists but it shouldn't. Remove it."
+        Remove-RoleGroup -Identity $Name -Confirm:$false -Force
+    }
+    # CASE: Role Group exists and it should, but has different values than the desired ones
+    elseif ($Ensure -eq 'Present' -and $currentRoleGroupConfig.Ensure -eq 'Present') {
+        Write-Verbose -Message "Role Group '$($Name)' already exists, but needs updating."
+        Remove-RoleGroup -Identity $Name -Confirm:$false -Force
+        Write-Verbose -Message "Setting Role Group $($Name) with values: $(Convert-M365DscHashtableToString -Hashtable $NewRoleGroupParams)"
+        
+        New-RoleGroup @NewRoleGroupParams
+    }
+    # CASE: Role Group exists and it should, but has different member values than the desired ones
+    elseif ($Ensure -eq 'Present' -and $currentRoleGroupConfig.Ensure -eq 'Present' -and $null -ne (Compare-Object -ReferenceObject $($currentRoleGroupConfig.Members) -DifferenceObject $Members)) {
+        Write-Verbose -Message "Role Group '$($Name)' already exists, but members need updating."
+        Write-Verbose -Message "Updating Role Group $($Name) members with values: $(Convert-M365DscHashtableToString -Hashtable $NewRoleGroupParams)"
+        
+        Update-RoleGroupMember -Identity $Name -Members $Members -Confirm:$false
+    }
+    # CASE: Role Assignment Policy exists and it should, but Roles attribute has different values than the desired ones
+    # Set-RoleGroup cannot change Roles attribute. Therefore we have to remove and recreate the assignment policies for the group if Roles attribute should be changed.
+    elseif ($Ensure -eq 'Present' -and $currentRoleGroupConfig.Ensure -eq 'Present' -and $null -ne (Compare-Object -ReferenceObject $($currentRoleGroupConfig.Roles) -DifferenceObject $Roles))
+    {
+        Write-Verbose -Message "Role Group '$($Name)' already exists, but roles attribute needs updating."
+        $differences = Compare-Object -ReferenceObject $($currentRoleGroupConfig.Roles) -DifferenceObject $Roles
+        foreach ($difference in $differences)
+        {
+            if ($difference.SideIndicator -eq '=>')
+            {
+                Write-Verbose -Message "Adding Role {$($difference.InputObject)} to Role Group {$Name}"
+                New-ManagementRoleAssignment -Role $($difference.InputObject) -SecurityGroup $Name
+            }
+            elseif ($difference.SideIndicator -eq '<=')
+            {
+                Write-Verbose -Message "Removing Role {$($difference.InputObject)} from Role Group {$Name}"
+                Remove-ManagementRoleAssignment -Identity "$($difference.InputObject)-$Name"
+            }
+        }
+    }
+}
+
+function Test-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateLength(1, 64)]
+        [System.String]
+        $Name,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [System.String[]]
+        $Members,
+
+        [Parameter()]
+        [System.String[]]
+        $Roles,
+
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
+    #Ensure the proper dependencies are installed in the current environment.
+    Confirm-M365DSCDependencies
+
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName -replace 'MSFT_', ''
+    $CommandName = $MyInvocation.MyCommand
+    $data = Format-M365DSCTelemetryParameters -ResourceName $ResourceName `
+        -CommandName $CommandName `
+        -Parameters $PSBoundParameters
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    Write-Verbose -Message "Testing Role Group configuration for $Name"
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    Write-Verbose -Message "Current Values: $(Convert-M365DscHashtableToString -Hashtable $CurrentValues)"
+    Write-Verbose -Message "Target Values: $(Convert-M365DscHashtableToString -Hashtable $PSBoundParameters)"
+
+    $ValuesToCheck = $PSBoundParameters
+    $ValuesToCheck.Remove('Credential') | Out-Null
+    $ValuesToCheck.Remove('ApplicationId') | Out-Null
+    $ValuesToCheck.Remove('TenantId') | Out-Null
+    $ValuesToCheck.Remove('CertificateThumbprint') | Out-Null
+    $ValuesToCheck.Remove('CertificatePath') | Out-Null
+    $ValuesToCheck.Remove('CertificatePassword') | Out-Null
+    $ValuesToCheck.Remove('ManagedIdentity') | Out-Null
+
+    $TestResult = Test-M365DSCParameterState -CurrentValues $CurrentValues `
+        -Source $($MyInvocation.MyCommand.Source) `
+        -DesiredValues $PSBoundParameters `
+        -ValuesToCheck $ValuesToCheck.Keys
+
+    Write-Verbose -Message "Test-TargetResource returned $TestResult"
+
+    return $TestResult
+}
+
+function Export-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param
+    (
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [System.String]
+        $CertificatePath,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $CertificatePassword,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
+    $ConnectionMode = New-M365DSCConnection -Workload 'ExchangeOnline' `
+        -InboundParameters $PSBoundParameters `
+        -SkipModuleReload $true
+
+    #Ensure the proper dependencies are installed in the current environment.
+    Confirm-M365DSCDependencies
+
+    #region Telemetry
+    $ResourceName = $MyInvocation.MyCommand.ModuleName -replace 'MSFT_', ''
+    $CommandName = $MyInvocation.MyCommand
+    $data = Format-M365DSCTelemetryParameters -ResourceName $ResourceName `
+        -CommandName $CommandName `
+        -Parameters $PSBoundParameters
+    Add-M365DSCTelemetryEvent -Data $data
+    #endregion
+
+    try {
+        [array]$AllRoleGroups = Get-RoleGroup 
+
+        $dscContent = ''
+
+        if ($AllRoleGroups.Length -eq 0) {
+            Write-Host $Global:M365DSCEmojiGreenCheckMark
+        }
+        else {
+            Write-Host "`r`n" -NoNewline
+        }
+        $i = 1
+        foreach ($RoleGroup in $AllRoleGroups) {
+            Write-Host "    |---[$i/$($AllRoleGroups.Count)] $($RoleGroup.Name)" -NoNewline
+
+            $Params = @{
+                Name                  = $RoleGroup.Name
+                Members               = $RoleGroup.Members
+                Roles                 = $RoleGroup.Roles
+                Credential            = $Credential
+                ApplicationId         = $ApplicationId
+                TenantId              = $TenantId
+                CertificateThumbprint = $CertificateThumbprint
+                CertificatePassword   = $CertificatePassword
+                Managedidentity       = $ManagedIdentity.IsPresent
+                CertificatePath       = $CertificatePath
+            }
+            $Results = Get-TargetResource @Params
+            $Results = Update-M365DSCExportAuthenticationResults -ConnectionMode $ConnectionMode `
+                -Results $Results
+            $currentDSCBlock = Get-M365DSCExportContentForResource -ResourceName $ResourceName `
+                -ConnectionMode $ConnectionMode `
+                -ModulePath $PSScriptRoot `
+                -Results $Results `
+                -Credential $Credential
+            $dscContent += $currentDSCBlock
+            Save-M365DSCPartialExport -Content $currentDSCBlock `
+                -FileName $Global:PartialExportFileName
+            Write-Host $Global:M365DSCEmojiGreenCheckMark
+            $i++
+        }
+        return $dscContent
+    }
+    catch {
+        Write-Host $Global:M365DSCEmojiRedX
+
+        New-M365DSCLogEntry -Message 'Error during Export:' `
+            -Exception $_ `
+            -Source $($MyInvocation.MyCommand.Source) `
+            -TenantId $TenantId `
+            -Credential $Credential
+
+        return ''
+    }
+}
+
+Export-ModuleMember -Function *-TargetResource
+
---
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.EXORoleGroup.Tests.ps1
@@ -0,0 +1,215 @@
+[CmdletBinding()]
+param(
+)
+$M365DSCTestFolder = Join-Path -Path $PSScriptRoot `
+    -ChildPath '..\..\Unit' `
+    -Resolve
+$CmdletModule = (Join-Path -Path $M365DSCTestFolder `
+        -ChildPath '\Stubs\Microsoft365.psm1' `
+        -Resolve)
+$GenericStubPath = (Join-Path -Path $M365DSCTestFolder `
+        -ChildPath '\Stubs\Generic.psm1' `
+        -Resolve)
+Import-Module -Name (Join-Path -Path $M365DSCTestFolder `
+        -ChildPath '\UnitTestHelper.psm1' `
+        -Resolve)
+
+$Global:DscHelper = New-M365DscUnitTestHelper -StubModule $CmdletModule `
+    -DscResource 'EXORoleGroup' -GenericStubModule $GenericStubPath
+Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
+    InModuleScope -ModuleName $Global:DscHelper.ModuleName -ScriptBlock {
+        Invoke-Command -ScriptBlock $Global:DscHelper.InitializeScript -NoNewScope
+
+        BeforeAll {
+            $secpasswd = ConvertTo-SecureString 'test@password1' -AsPlainText -Force
+            $Credential = New-Object System.Management.Automation.PSCredential ('tenantadmin@mydomain.com', $secpasswd)
+
+            $Global:PartialExportFileName = 'c:\TestPath'
+
+            Mock -CommandName Save-M365DSCPartialExport -MockWith {
+            }
+
+            Mock -CommandName New-M365DSCConnection -MockWith {
+                return 'Credentials'
+            }
+
+            Mock -CommandName Get-PSSession -MockWith {
+            }
+
+            Mock -CommandName Remove-PSSession -MockWith {
+            }
+
+            # Mock Write-Host to hide output during the tests
+            Mock -CommandName Write-Host -MockWith {
+            }
+            Mock -CommandName Get-RoleGroup -MockWith {
+                return @{
+                    Name                = 'Contoso Differet Role Group'
+                    Members             = 'Exchange Administrator'
+                    Roles               = 'Address Lists'
+                    Description         = 'This is the Different Contoso Role Group'
+                }
+            }
+
+            Mock -CommandName New-RoleGroup -MockWith {
+                return @{
+                    Name        = 'Contoso Role Group'
+                    Members     = 'Exchange Administrator'
+                    Roles       = 'Address Lists'
+                    Description = 'This is the Contoso Role Group'
+                    Ensure      = 'Present'
+                    Credential  = $Credential
+                }
+            }
+        }
+
+        # Test contexts
+        Context -Name 'Role Group should exist. Role Group is missing. Test should fail.' -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Name        = 'Contoso Role Group'
+                    Members     = 'Exchange Administrator'
+                    Roles       = 'Address Lists'
+                    Description = 'This is the Contoso Role Group'
+                    Ensure      = 'Present'
+                    Credential  = $Credential
+                }
+
+                Mock -CommandName Get-RoleGroup -MockWith {
+                    return @{
+                        Name                = 'Contoso Differet Role Group'
+                        Members             = 'Exchange Administrator'
+                        Roles               = 'Address Lists'
+                        Description         = 'This is the Different Contoso Role Group'
+                    }
+                }
+
+                Mock -CommandName New-RoleGroup -MockWith {
+                    return @{
+                        Name        = 'Contoso Role Group'
+                        Members     = 'Exchange Administrator'
+                        Roles       = 'Address Lists'
+                        Description = 'This is the Contoso Role Group'
+                        Ensure      = 'Present'
+                        Credential  = $Credential
+                    }
+                }
+            }
+
+            It 'Should return false from the Test method' {
+                Test-TargetResource @testParams | Should -Be $false
+            }
+
+            It 'Should call the Set method' {
+                Set-TargetResource @testParams
+            }
+
+            It 'Should return Absent from the Get method' {
+                (Get-TargetResource @testParams).Ensure | Should -Be 'Absent'
+            }
+        }
+
+        Context -Name 'Role Group should exist. Role Group exists. Test should pass.' -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Name        = 'Contoso Role Group'
+                    Members     = 'Exchange Administrator'
+                    Roles       = 'Address Lists'
+                    Description = 'This is the Contoso Role Group'
+                    Ensure      = 'Present'
+                    Credential  = $Credential
+                }
+
+                Mock -CommandName Get-RoleGroup -MockWith {
+                    return @{
+                        Name        = 'Contoso Role Group'
+                        Members     = 'Exchange Administrator'
+                        Roles       = 'Address Lists'
+                        Description = 'This is the Contoso Role Group'
+                    }
+                }
+                Mock -CommandName Get-RoleGroupMember -MockWith {
+                    return @{
+                        DisplayName = 'Exchange Administrator'
+                    }
+                }
+            }
+
+            It 'Should return true from the Test method' {
+                Test-TargetResource @testParams | Should -Be $true
+            }
+
+            It 'Should return Present from the Get Method' {
+                (Get-TargetResource @testParams).Ensure | Should -Be 'Present'
+            }
+        }
+
+        Context -Name 'Role Group should exist. Role Group exists, Description mismatch. Test should fail.' -Fixture {
+            BeforeAll {
+                $testParams = @{
+                    Name        = 'Contoso Role Group'
+                    Members     = 'Exchange Administrator'
+                    Roles       = 'Address Lists'
+                    Description = 'This is the Contoso Role Group'
+                    Ensure      = 'Present'
+                    Credential  = $Credential
+                }
+
+                Mock -CommandName Get-RoleGroup -MockWith {
+                    return @{
+                        Name        = 'Contoso Role Group'
+                        Members     = 'Exchange Administrator'
+                        Roles       = 'Address Lists'
+                        Description = 'This is the Different Contoso Role Group'
+                    }
+                }
+
+                Mock -CommandName New-RoleGroup -MockWith {
+                    return @{
+                        Name        = 'Contoso Role Group'
+                        Members     = 'Exchange Administrator'
+                        Roles       = 'Address Lists'
+                        Description = 'This is the Contoso Role Group'
+                        Ensure      = 'Present'
+                        Credential  = $Credential
+                    }
+                }
+            }
+
+            It 'Should return false from the Test method' {
+                Test-TargetResource @testParams | Should -Be $false
+            }
+
+            It 'Should call the Set method' {
+                Set-TargetResource @testParams
+            }
+        }
+
+        Context -Name 'ReverseDSC Tests' -Fixture {
+            BeforeAll {
+                $Global:CurrentModeIsExport = $true
+                $Global:PartialExportFileName = "$(New-Guid).partial.ps1"
+                $testParams = @{
+                    Credential = $Credential
+                }
+
+                $RoleGroup = @{
+                    Name        = 'Contoso Role Group'
+                    Members     = 'Exchange Administrator'
+                    Roles       = 'Address Lists'
+                    Description = 'This is the Contoso Role Group'
+                }
+                Mock -CommandName Get-RoleGroup -MockWith {
+                    return $RoleGroup
+                }
+            }
+
+            It 'Should Reverse Engineer resource from the Export method when single' {
+                $result = Export-TargetResource @testParams
+                $result | Should -Not -BeNullOrEmpty
+            }
+        }
+    }
+}
+
+Invoke-Command -ScriptBlock $Global:DscHelper.CleanupScript -NoNewScope
