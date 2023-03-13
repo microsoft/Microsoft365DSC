f3fabee35373cbeb3f96c0ee2c3781ca1b645fb4
William-francillette <william.francillette@outlook.com>
2023-03-13 19:04:21 +0000

    Unit test + stub

--- Tests/Unit/Stubs/Microsoft365.psm1
+++ Tests/Unit/Stubs/Microsoft365.psm1
@@ -48688,3 +48688,84 @@ function Update-MgDeviceManagementWindowFeatureUpdateProfileAssignment
 
 #endregion
 
+#region MgDirectoryObject
+
+function Get-MgDirectoryObject
+{
+    [CmdletBinding()]
+    param(
+
+        [Parameter()]
+        [System.String[]]
+        $DirectoryObjectId,
+
+        [Parameter()]
+        [PSObject]
+        $InputObject,
+
+        [Parameter()]
+        [System.String[]]
+        $ExpandProperty,
+
+        [Parameter()]
+        [System.String[]]
+        $Property,
+
+        [Parameter()]
+        [System.String]
+        $Filter,
+
+        [Parameter()]
+        [System.String]
+        $Search,
+
+        [Parameter()]
+        [System.Int32]
+        $Skip,
+
+        [Parameter()]
+        [System.String[]]
+        $Sort,
+
+        [Parameter()]
+        [System.Int32]
+        $Top,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $Break,
+
+        [Parameter()]
+        [PSObject[]]
+        $HttpPipelineAppend,
+
+        [Parameter()]
+        [PSObject[]]
+        $HttpPipelinePrepend,
+
+        [Parameter()]
+        [System.Uri]
+        $Proxy,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $ProxyCredential,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $ProxyUseDefaultCredentials,
+
+        [Parameter()]
+        [System.Int32]
+        $PageSize,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $All,
+
+        [Parameter()]
+        [System.String]
+        $CountVariable
+    )
+}
+#endregion
