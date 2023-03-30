bc413d3a59c29220f69dc297ac0931574c9410ee
Nik Charlebois <nicharl@microsoft.com>
2023-03-30 11:57:42 -0400

    Update 2-CreateNewAdministrativeUnit.ps1

--- Modules/Microsoft365DSC/Examples/Resources/AADAdministrativeUnit/2-CreateNewAdministrativeUnit.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/AADAdministrativeUnit/2-CreateNewAdministrativeUnit.ps1
@@ -2,18 +2,6 @@
 This example is used to test new resources and showcase the usage of new resources being worked on.
 It is not meant to use as a production baseline.
 #>
-
-Configuration Example
-{
-    param
-    (
-        [Parameter(Mandatory = $true)]
-        [PSCredential]
-        $credsGlobalAdmin
-    )
-
-    Import-DscResource -ModuleName Microsoft365DSC
-
 Configuration Example
 {
     param
