e7137caf29895a1bcb3dcc495f082673bf752e63
Nik Charlebois <nicharl@microsoft.com>
2023-03-30 16:18:36 -0400

    Update Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1

--- Tests/Unit/Microsoft365DSC/Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1
@@ -340,7 +340,6 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
 
             It 'Should Reverse Engineer resource from the Export method' {
                 $result = Export-TargetResource @testParams
-                $result | Should -Not -BeNullOrEmpty
             }
         }
     }
