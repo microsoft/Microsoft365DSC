86e723f7ce40a37e410f28e58bf958bac51ac356
Sandro Lanfranchi <sandro.lanfranchi@swisscom.com>
2023-04-25 07:40:36 +0200

    Add Exemple for AuthenticatinPolicy

--- Modules/Microsoft365DSC/Examples/Resources/AADAuthenticationMethodPolicy/1-AADAuthenticationMethodPolicy-Example.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/AADAuthenticationMethodPolicy/1-AADAuthenticationMethodPolicy-Example.ps1
@@ -12,10 +12,32 @@ Configuration Example
     )
     Import-DscResource -ModuleName Microsoft365DSC
 
-    node localhost
+    Node localhost
     {
-        AADAuthenticationMethodPolicy 'Example'
+        AADAuthenticationMethodPolicy "AADAuthenticationMethodPolicy-Authentication Methods Policy"
         {
+            ApplicationId           = $ConfigurationData.NonNodeData.ApplicationId;
+            CertificateThumbprint   = $ConfigurationData.NonNodeData.CertificateThumbprint;
+            Description             = "The tenant-wide policy that controls which authentication methods are allowed in the tenant, authentication method registration requirements, and self-service password reset settings";
+            DisplayName             = "Authentication Methods Policy";
+            Ensure                  = "Present";
+            Id                      = "authenticationMethodsPolicy";
+            PolicyMigrationState    = "preMigration";
+            PolicyVersion           = "1.4";
+            RegistrationEnforcement = MSFT_MicrosoftGraphregistrationEnforcement{
+                AuthenticationMethodsRegistrationCampaign = MSFT_MicrosoftGraphAuthenticationMethodsRegistrationCampaign{
+                    SnoozeDurationInDays = 1
+                    IncludeTargets = @(
+                        MSFT_MicrosoftGraphAuthenticationMethodsRegistrationCampaignIncludeTarget{
+                            TargetedAuthenticationMethod = 'microsoftAuthenticator'
+                            TargetType = 'group'
+                            Id = 'all_users'
+                        }
+                    )
+                    State = 'default'
+                }
+                        };
+            TenantId                = $ConfigurationData.NonNodeData.TenantId;
         }
     }
 }
