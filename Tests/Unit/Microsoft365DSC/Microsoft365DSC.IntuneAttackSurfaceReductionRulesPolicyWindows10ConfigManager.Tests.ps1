9bc2994154ac14b942c61b3656d98c17fe0ec4b2
Nik Charlebois <nicharl@microsoft.com>
2023-07-11 08:18:35 -0400

    Update Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1

--- Tests/Unit/Microsoft365DSC/Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.IntuneAttackSurfaceReductionRulesPolicyWindows10ConfigManager.Tests.ps1
@@ -50,10 +50,6 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
                 }
             }
 
-            Mock -CommandName Get-M365DSCDeviceManagementConfigurationPolicyAssignment -MockWith {
-                return $null
-            }
-
             # Mock Write-Host to hide output during the tests
             Mock -CommandName Write-Host -MockWith {
             }
