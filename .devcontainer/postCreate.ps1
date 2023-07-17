042a03ba93427a44bfc0599a5dd1967282dd1927
Fabian Franz Steiner <75947402+fasteiner@users.noreply.github.com>
2023-07-17 16:17:09 +0200

    Add files via upload

---
+++ .devcontainer/postCreate.ps1
@@ -0,0 +1,10 @@
+Write-Host -Fore Green 'Starting postCreate.ps1'
+
+Write-Host -Fore Green 'Making pwsh our default shell'
+& sudo chsh -s /usr/bin/pwsh vscode
+
+Write-Host -Fore Green 'Perform Initial Dotnet Publish'
+& dotnet build
+
+Write-Host -Fore Green 'Install Pester'
+Install-Module Pester -AllowPrerelease -Force
