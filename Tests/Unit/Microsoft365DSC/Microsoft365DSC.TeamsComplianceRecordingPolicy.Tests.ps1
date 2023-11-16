d4cc1dee11cc4611c4fc16ebdda76d5e0bd5aede
Philippe Kernevez <pkernevez@kleis.ch>
2023-11-16 11:01:29 +0100

    Fix 2nd part

--- Tests/Unit/Microsoft365DSC/Microsoft365DSC.TeamsComplianceRecordingPolicy.Tests.ps1
+++ Tests/Unit/Microsoft365DSC/Microsoft365DSC.TeamsComplianceRecordingPolicy.Tests.ps1
@@ -124,7 +124,7 @@ Describe -Name $Global:DscHelper.DescribeHeader -Fixture {
                 $Result = (Get-TargetResource @testParams)
                 $Result.Ensure | Should -Be 'Present'
                 $Result.ComplianceRecordingApplications.Length | Should -Be 1
-                $Result.ComplianceRecordingApplications[0].Id | Should -Be '123456'
+                $Result.ComplianceRecordingApplications[0] | Should -Be '123456'
                 Should -Invoke -CommandName Get-CsTeamsComplianceRecordingPolicy -Exactly 1
                 Should -Invoke -CommandName Get-CsTeamsComplianceRecordingApplication -ParameterFilter {$Filter -eq 'FakeStringValue/*'} -Exactly 1
 
