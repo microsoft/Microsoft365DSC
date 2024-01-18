f0f239669282a048a55bbeeaefdd31555f09bd17
NikCharlebois <nicharl@microsoft.com>
2024-01-18 15:13:24 +0000

    Updated {Update} AAD Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Remove.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Remove.Tests.ps1
@@ -102,9 +102,9 @@
                     Ensure               = "Absent";
                     Credential           = $Credscredential;
                 }
-                AADConditionalAccessPolicy 'Allin-example'
+                AADConditionalAccessPolicy 'ConditionalAccessPolicy'
                 {
-                    DisplayName                          = 'Allin-example'
+                    DisplayName                          = 'Example CAP'
                     Ensure                               = 'Absent'
                     Credential                           = $Credscredential
                 }
@@ -219,50 +219,6 @@
                                 type        = 'afterDateTime'
                             }
                     };
-                }
-                AADRoleSetting '28b253d8-cde5-471f-a331-fe7320023cdd'
-                {
-                    ActivateApprover                                          = @();
-                    ActivationMaxDuration                                     = "PT8H";
-                    ActivationReqJustification                                = $False; # Updated Property
-                    ActivationReqMFA                                          = $False;
-                    ActivationReqTicket                                       = $False;
-                    ActiveAlertNotificationAdditionalRecipient                = @();
-                    ActiveAlertNotificationDefaultRecipient                   = $True;
-                    ActiveAlertNotificationOnlyCritical                       = $False;
-                    ActiveApproveNotificationAdditionalRecipient              = @();
-                    ActiveApproveNotificationDefaultRecipient                 = $True;
-                    ActiveApproveNotificationOnlyCritical                     = $False;
-                    ActiveAssigneeNotificationAdditionalRecipient             = @();
-                    ActiveAssigneeNotificationDefaultRecipient                = $True;
-                    ActiveAssigneeNotificationOnlyCritical                    = $False;
-                    ApprovaltoActivate                                        = $False;
-                    AssignmentReqJustification                                = $True;
-                    AssignmentReqMFA                                          = $False;
-                    Displayname                                               = "Application Administrator";
-                    ElegibilityAssignmentReqJustification                     = $False;
-                    ElegibilityAssignmentReqMFA                               = $False;
-                    EligibleAlertNotificationAdditionalRecipient              = @();
-                    EligibleAlertNotificationDefaultRecipient                 = $True;
-                    EligibleAlertNotificationOnlyCritical                     = $False;
-                    EligibleApproveNotificationAdditionalRecipient            = @();
-                    EligibleApproveNotificationDefaultRecipient               = $True;
-                    EligibleApproveNotificationOnlyCritical                   = $False;
-                    EligibleAssigneeNotificationAdditionalRecipient           = @();
-                    EligibleAssigneeNotificationDefaultRecipient              = $True;
-                    EligibleAssigneeNotificationOnlyCritical                  = $False;
-                    EligibleAssignmentAlertNotificationAdditionalRecipient    = @();
-                    EligibleAssignmentAlertNotificationDefaultRecipient       = $True;
-                    EligibleAssignmentAlertNotificationOnlyCritical           = $False;
-                    EligibleAssignmentAssigneeNotificationAdditionalRecipient = @();
-                    EligibleAssignmentAssigneeNotificationDefaultRecipient    = $True;
-                    EligibleAssignmentAssigneeNotificationOnlyCritical        = $False;
-                    ExpireActiveAssignment                                    = "P180D";
-                    ExpireEligibleAssignment                                  = "P365D";
-                    PermanentActiveAssignmentisExpirationRequired             = $False;
-                    PermanentEligibleAssignmentisExpirationRequired           = $False;
-                    Credential                                                = $Credscredential
-                    Ensure                                                    = 'Absent'
                 }
                 AADSecurityDefaults 'Defaults'
                 {
