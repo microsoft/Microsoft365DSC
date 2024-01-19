b4f62f057d46cc71c8790397587d304461969365
NikCharlebois <nicharl@microsoft.com>
2024-01-19 14:48:51 +0000

    Updated {Create} AAD Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Create.Tests.ps1
@@ -227,7 +227,7 @@
                     GroupTypes      = @("Unified")
                     MailNickname    = "M365DSC"
                     Visibility      = "Private"
-                    Owners          = @("admin@$Domain", "AdeleV@$Domain")
+                    Owners          = @("admin@$Domain", "AdeleV@$Domain", "GitHubIntegration@$Domain")
                     Ensure          = "Present"
                     Credential      = $Credscredential
                 }
