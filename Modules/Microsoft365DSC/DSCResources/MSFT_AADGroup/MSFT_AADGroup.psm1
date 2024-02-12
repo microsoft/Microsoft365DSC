d3d81cc8761aca75bb0fd0f4ab0084c0d6123b67
Vasily <stvlab@gmail.com>
2024-02-12 13:44:16 +0100

    Update MSFT_AADGroup.psm1
    fixed and removed an extra bracket in line #1103
    
    if ($matchConditionFound -or $Filter -like "*endsWith*")

--- Modules/Microsoft365DSC/DSCResources/MSFT_AADGroup/MSFT_AADGroup.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADGroup/MSFT_AADGroup.psm1
@@ -1100,7 +1100,7 @@ function Export-TargetResource
         }
 
         # If any attribute matches, add parameters to $ExportParameters
-        if ($matchConditionFound -or $Filter -like "*endsWith*")) {
+        if ($matchConditionFound -or $Filter -like "*endsWith*") {
             $ExportParameters.Add('CountVariable', 'count')
             $ExportParameters.Add('ConsistencyLevel', 'eventual')
         }
