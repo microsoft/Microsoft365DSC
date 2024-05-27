c7daf5c4a9d8f6ac1ee53edb884c4b344bd58416
NikCharlebois <nicharl@microsoft.com>
2024-05-27 13:48:39 +0000

    Updated {Create} Intune Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
@@ -82,6 +82,23 @@
                     Ensure             = 'Present'
                     Credential         = $Credscredential
                 }
+                IntuneAppConfigurationDevicePolicy 'IntuneAppConfigurationDevicePolicy-Example'
+                {
+                    Credential            = $Credscredential;
+                    Assignments           = @();
+                    Description           = "";
+                    DisplayName           = "Example";
+                    Ensure                = "Present";
+                    Id                    = "0000000-0000-0000-0000-000000000000";
+                    ConnectedAppsEnabled  = $true;
+                    PackageId             = "app:com.microsoft.office.outlook"
+                    PayloadJson           = "Base64 encoded settings"
+                    PermissionActions     = @()
+                    ProfileApplicability  = "default"
+                    RoleScopeTagIds       = @("0");
+                    TargetedMobileApps    = @("<Mobile App Id>");
+                    TenantId              = $OrganizationName;
+                }
                 IntuneAppConfigurationPolicy 'AddAppConfigPolicy'
                 {
                     DisplayName          = 'ContosoNew'
