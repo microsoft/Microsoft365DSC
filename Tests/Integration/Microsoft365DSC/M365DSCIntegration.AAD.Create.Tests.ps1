8d610913159d3aa161cbfb0087e54d27e5eb391d
NikCharlebois <nicharl@microsoft.com>
2024-07-15 17:58:19 +0000

    Updated {Create} AAD Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Create.Tests.ps1
@@ -240,9 +240,9 @@
                     ApplicationId         = $ApplicationId;
                     CatalogId             = "My Catalog";
                     CertificateThumbprint = $CertificateThumbprint;
-                    DisplayName           = "DSCGroup";
+                    DisplayName           = "Finance Team";
                     OriginSystem          = "AADGroup";
-                    OriginId              = '849b3661-61a8-44a8-92e7-fcc91d296235'
+                    OriginId              = '50523ab8-a3d2-4b4f-a77d-16bd1abd328f'
                     Ensure                = "Present";
                     IsPendingOnboarding   = $False;
                     TenantId              = $TenantId;
