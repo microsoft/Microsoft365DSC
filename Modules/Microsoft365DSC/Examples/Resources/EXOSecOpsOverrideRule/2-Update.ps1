342132849be46aff4f7b4477f45e315ef4c97b22
ritikmit <ritikmittal@microsoft.com>
2024-09-05 10:51:25 +0530

    Fixed example

--- Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/2-Update.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/2-Update.ps1
@@ -24,12 +24,10 @@ Configuration Example
         EXOSecOpsOverrideRule "EXOSecOpsOverrideRule-_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245"
         {
             Comment                                  = "TestComment";
-            Credential                               = $Credscredential;
             Ensure                                   = "Present";
             Identity                                 = "_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245";
             Policy                                   = "40528418-717d-4368-a1ae-7912918f8a1f";
             Credential                               = $Credscredential;
-            Domains                                  = @("fabrikam.com","wingtiptoys.com");
             ApplicationId                            = $ApplicationId
             TenantId                                 = $TenantId
             CertificateThumbprint                    = $CertificateThumbprint
