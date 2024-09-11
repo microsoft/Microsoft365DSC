6ef7beb07fb4231245041863ce8114b6c9407dbc
NikCharlebois <nicharl@microsoft.com>
2024-09-11 06:39:21 +0000

    Updated {Update} EXO Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Update.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Update.Tests.ps1
@@ -575,6 +575,51 @@
                     TenantId              = $TenantId
                     CertificateThumbprint = $CertificateThumbprint
                 }
+                EXOMailboxCalendarConfiguration 'EXOMailboxCalendarConfiguration-Test'
+                {
+                    AgendaMailIntroductionEnabled            = $True;
+                    AutoDeclineWhenBusy                      = $False;
+                    ConversationalSchedulingEnabled          = $True;
+                    CreateEventsFromEmailAsPrivate           = $True;
+                    DefaultMinutesToReduceLongEventsBy       = 10;
+                    DefaultMinutesToReduceShortEventsBy      = 6; # Updated Property
+                    DefaultOnlineMeetingProvider             = "TeamsForBusiness";
+                    DefaultReminderTime                      = "00:15:00";
+                    DeleteMeetingRequestOnRespond            = $True;
+                    DiningEventsFromEmailEnabled             = $True;
+                    Ensure                                   = "Present";
+                    EntertainmentEventsFromEmailEnabled      = $True;
+                    EventsFromEmailEnabled                   = $True;
+                    FirstWeekOfYear                          = "FirstDay";
+                    FlightEventsFromEmailEnabled             = $True;
+                    HotelEventsFromEmailEnabled              = $True;
+                    Identity                                 = "admin@$TenantId";
+                    InvoiceEventsFromEmailEnabled            = $True;
+                    LocationDetailsInFreeBusy                = "Desk";
+                    PackageDeliveryEventsFromEmailEnabled    = $False;
+                    PreserveDeclinedMeetings                 = $False;
+                    RemindersEnabled                         = $True;
+                    ReminderSoundEnabled                     = $True;
+                    RentalCarEventsFromEmailEnabled          = $True;
+                    ServiceAppointmentEventsFromEmailEnabled = $True;
+                    ShortenEventScopeDefault                 = "None";
+                    ShowWeekNumbers                          = $False;
+                    TimeIncrement                            = "ThirtyMinutes";
+                    UseBrightCalendarColorThemeInOwa         = $False;
+                    WeatherEnabled                           = "FirstRun";
+                    WeatherLocationBookmark                  = 0;
+                    WeatherLocations                         = @();
+                    WeatherUnit                              = "Default";
+                    WeekStartDay                             = "Sunday";
+                    WorkDays                                 = "Monday, Tuesday";
+                    WorkingHoursEndTime                      = "17:00:00";
+                    WorkingHoursStartTime                    = "08:00:00";
+                    WorkingHoursTimeZone                     = "Pacific Standard Time";
+                    WorkspaceUserEnabled                     = $False;
+                    ApplicationId                            = $ApplicationId
+                    TenantId                                 = $TenantId
+                    CertificateThumbprint                    = $CertificateThumbprint
+                }
                 EXOMailboxCalendarFolder 'JohnCalendarFolder'
                 {
                     DetailLevel          = "AvailabilityOnly";
