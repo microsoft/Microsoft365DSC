bc164a4bca8f9e6fc761d357cd5077441f049510
ritikmit <88980817+ritikmit@users.noreply.github.com>
2024-09-11 11:51:53 +0530

    Update 1-Create.ps1

--- Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/1-Create.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/1-Create.ps1
@@ -24,7 +24,6 @@ Configuration Example
         EXOSecOpsOverrideRule "EXOSecOpsOverrideRule-_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245"
         {
             Comment              = "TestComment";
-            Credential           = $Credscredential;
             Ensure               = "Present";
             Identity             = "_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245";
             Policy               = "40528418-717d-4368-a1ae-7912918f8a1f";
