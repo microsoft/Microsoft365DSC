1741c5b1627ec117eedf418df3b1cf1083110262
NikCharlebois <nicharl@microsoft.com>
2024-09-11 06:17:28 +0000

    Updated {Update} EXO Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Update.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Update.Tests.ps1
@@ -348,6 +348,16 @@
                     TenantId              = $TenantId
                     CertificateThumbprint = $CertificateThumbprint
                 }
+                EXOFocusedInbox 'EXOFocusedInbox-Test'
+                {
+                    Ensure                       = "Present";
+                    FocusedInboxOn               = $False; # Updated Property
+                    FocusedInboxOnLastUpdateTime = "1/1/0001 12:00:00 AM";
+                    Identity                     = "admin@$TenantId";
+                    ApplicationId                = $ApplicationId;
+                    TenantId                     = $TenantId;
+                    CertificateThumbprint        = $CertificateThumbprint;
+                }
                 EXOGlobalAddressList 'ConfigureGlobalAddressList'
                 {
                     Name                         = "Contoso Human Resources in Washington"
