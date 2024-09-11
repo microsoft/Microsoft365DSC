377a5bf15c3502b9549c77dd871d7d6e79219645
NikCharlebois <nicharl@microsoft.com>
2024-09-11 07:05:49 +0000

    Updated {Create} EXO Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Create.Tests.ps1
@@ -884,6 +884,13 @@
                     TenantId              = $TenantId
                     CertificateThumbprint = $CertificateThumbprint
                 }
+                EXOSecOpsOverrideRule 'EXOSecOpsOverrideRule-_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245'
+                {
+                    Comment              = "TestComment";
+                    Ensure               = "Present";
+                    Identity             = "_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245";
+                    Policy               = "40528418-717d-4368-a1ae-7912918f8a1f";
+                }
                 EXOSharedMailbox 'SharedMailbox'
                 {
                     DisplayName        = "Integration"
