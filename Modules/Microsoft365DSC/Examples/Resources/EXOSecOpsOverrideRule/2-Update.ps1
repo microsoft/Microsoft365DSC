97d11fe9ce6c12ba734539618300b43896eab2c1
ritikmit <88980817+ritikmit@users.noreply.github.com>
2024-09-11 11:52:09 +0530

    Update 2-Update.ps1

--- Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/2-Update.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/EXOSecOpsOverrideRule/2-Update.ps1
@@ -27,7 +27,6 @@ Configuration Example
             Ensure                                   = "Present";
             Identity                                 = "_Exe:SecOpsOverrid:ca3c51ac-925c-49f4-af42-43e26b874245";
             Policy                                   = "40528418-717d-4368-a1ae-7912918f8a1f";
-            Credential                               = $Credscredential;
             ApplicationId                            = $ApplicationId
             TenantId                                 = $TenantId
             CertificateThumbprint                    = $CertificateThumbprint
