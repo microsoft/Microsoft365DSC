b3897b014b89e04c6d0319158b8bdd334c7155de
Yorick Kuijs <ykuijs@hotmail.com>
2024-09-18 13:06:48 +0200

    Implementing [SCSensitityLabel] Auto Labeling missing in export #3784

--- Tests/Unit/Stubs/Microsoft365.psm1
+++ Tests/Unit/Stubs/Microsoft365.psm1
@@ -64126,6 +64126,31 @@ function Get-DlpComplianceRule
         $IncludeExecutionRuleGuids
     )
 }
+function Get-DlpSensitiveInformationType
+{
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [System.Object]
+        $IncludeElements,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $IncludeDetails,
+
+        [Parameter()]
+        [System.Object]
+        $Identity,
+
+        [Parameter()]
+        [System.Object]
+        $Organization,
+
+        [Parameter()]
+        [System.Object]
+        $Capability
+    )
+}
 function Get-FilePlanPropertyAuthority
 {
     [CmdletBinding()]
