af28151061c9c2c2e4a8de7d03df3c33d84a993c
NikCharlebois <nicharl@microsoft.com>
2024-09-19 17:19:03 +0000

    Updated {Update} EXO Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Remove.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.EXO.Remove.Tests.ps1
@@ -115,6 +115,20 @@
                     TenantId              = $TenantId
                     CertificateThumbprint = $CertificateThumbprint
                 }
+                EXOATPProtectionPolicyRule 'EXOATPProtectionPolicyRule-Strict Preset Security Policy'
+                {
+                    Comments                = "Built-in Strict Preset Security Policy";
+                    Enabled                 = $False;
+                    Identity                = "Strict Preset Security Policy";
+                    Name                    = "Strict Preset Security Policy";
+                    Priority                = 0;
+                    SafeAttachmentPolicy    = "Strict Preset Security Policy1725468967835";
+                    SafeLinksPolicy         = "Strict Preset Security Policy1725468969412";
+                    Ensure                  = "Absent"
+                    ApplicationId           = $ApplicationId
+                    TenantId                = $TenantId
+                    CertificateThumbprint   = $CertificateThumbprint
+                }
                 EXOAuthenticationPolicy 'ConfigureAuthenticationPolicy'
                 {
                     Identity                            = "Block Basic Auth"
