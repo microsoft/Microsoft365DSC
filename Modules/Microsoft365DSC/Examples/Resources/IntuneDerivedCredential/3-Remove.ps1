169384cd4942ff11659b6467d8d4bfb73316fd4f
Kajalp1079 <kajalpatel2014@outlook.com>
2024-10-05 19:27:31 -0700

    Draft Intune DSC Derived Credential

--- Modules/Microsoft365DSC/Examples/Resources/IntuneDerivedCredential/3-Remove.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/IntuneDerivedCredential/3-Remove.ps1
@@ -1,36 +1,32 @@
-<#
-This example is used to test new resources and showcase the usage of new resources being worked on.
-It is not meant to use as a production baseline.
-#>
-
-Configuration Example
-{
+<# This example is used to test new resources and showcase the usage of new resources being worked on. It is not meant to use as a production baseline. #>
+Configuration Example {
     param(
         [Parameter()]
-        [System.String]
-        $ApplicationId,
+        [System.String] $ApplicationId,
 
         [Parameter()]
-        [System.String]
-        $TenantId,
+        [System.String] $TenantId,
 
         [Parameter()]
-        [System.String]
-        $CertificateThumbprint
+        [System.String] $CertificateThumbprint
     )
-    Import-DscResource -ModuleName Microsoft365DSC
-    node localhost
-    {
-        IntuneDerivedCredential "IntuneDerivedCredential-Data Management"
-        {
-            Id                   = "a1fc9fe2-728d-4867-9a72-a61e18f8c606";
-            DisplayName          = "Custom Data Management";
-            Ensure               = "Present";
-            HelpUrl              = "https://www.microsoft.com";
-            Issuer               = "DISA Purebred";
-            NotificationType     = "Email";
-            ThresholdPercentage  = 0;
-        }
 
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost {
+        IntuneDerivedCredential "IntuneDerivedCredential-DataManagement" {
+            Id                  = "a1fc9fe2-728d-4867-9a72-a61e18f8c606"
+            DisplayName         = "Custom Data Management"
+            Ensure              = "Present"
+            HelpUrl             = "https://www.microsoft.com"
+            Issuer              = "DISA Purebred"
+            NotificationType    = "Email"
+            ThresholdPercentage = 0
+            Header              = @(
+                [PSCustomObject]@{ Key = 'HeaderKey1'; Value = 'HeaderValue1' }
+                [PSCustomObject]@{ Key = 'HeaderKey2'; Value = 'HeaderValue2' }
+            )
+        }
     }
 }
+
