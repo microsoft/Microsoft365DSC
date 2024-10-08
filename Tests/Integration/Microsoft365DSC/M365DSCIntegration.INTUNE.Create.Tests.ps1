a12f665aa56448cd15c03b594d28bf867388c758
NikCharlebois <nicharl@microsoft.com>
2024-10-08 10:47:54 +0000

    Updated {Create} Intune Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
@@ -2528,6 +2528,7 @@
                                 deviceAndAppManagementAssignmentFilterType = 'none'
                                 dataType = '#microsoft.graph.allDevicesAssignmentTarget'
                                 intent = 'required'
+                            }
                             MSFT_DeviceManagementMobileAppAssignment{
                                 deviceAndAppManagementAssignmentFilterType = 'none'
                                 source = 'direct'
@@ -2535,7 +2536,7 @@
                                 groupId = '57b5e81c-85bb-4644-a4fd-33b03e451c89'
                                 intent = 'required'
                             }
-                        });
+                        );
                     Categories           = @(MSFT_DeviceManagementMobileAppCategory {
                             id  = '1bff2652-03ec-4a48-941c-152e93736515'
                             displayName = 'Kajal 3'
