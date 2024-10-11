ab301f613e3b9b3faede60edd2ff4753139fb517
Jason Hiskey <54078279+jahiskey@users.noreply.github.com>
2024-10-11 10:36:52 -0700

    Finished config now testing

---
+++ Modules/Microsoft365DSC/Examples/Resources/IntuneDeviceManagmentAndroidDeviceOwnerEnrollmentProfile/1-Create.ps1
@@ -0,0 +1,40 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint
+    )
+
+    Import-DscResource -ModuleName Microsoft365DSC
+    node localhost
+    {
+        IntuneDeviceManagementAndroidDeviceOwnerEnrollmentProfile 'CreateProfile'
+        {
+            Id                      = "164655f7-1232-4d56-ae8f-b095196a0309";
+            DisplayName             = "Android Owner Enrollment Profile"
+            Description             = "Profile for enrolling Android devices"
+            TokenExpirationDateTime = "2024-12-31T23:59:59Z"
+            TokenValue              = "your-token-value"
+            EnrollmentMode          = "corporateOwnedWorkProfile"
+            QrCodeContent           = "your-qr-code-content"
+            WifiSsid                = "your-wifi-ssid"
+            WifiPassword            = "your-wifi-password"
+            WifiSecurityType        = "wpa"
+            Ensure                  = "Present";
+        }
+    }
+}
---
+++ Modules/Microsoft365DSC/Examples/Resources/IntuneDeviceManagmentAndroidDeviceOwnerEnrollmentProfile/2-Update.ps1
@@ -0,0 +1,39 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+    node localhost
+    {
+        IntuneDeviceManagementAndroidDeviceOwnerEnrollmentProfile 'UpdateProfile'
+        {
+            Id                      = "164655f7-1232-4d56-ae8f-b095196a0309";
+            DisplayName             = "Updated Android Owner Enrollment Profile"
+            Description             = "Updated Profile for enrolling Android devices"
+            TokenExpirationDateTime = "2024-12-31T23:59:59Z"
+            TokenValue              = "your-updated-token-value"
+            EnrollmentMode          = "corporateOwnedWorkProfile"
+            QrCodeContent           = "your-updated-qr-code-content"
+            WifiSsid                = "your-updated-wifi-ssid"
+            WifiPassword            = "your-updated-wifi-password"
+            WifiSecurityType        = "wpa"
+            Ensure                  = "Present";
+        }
+    }
+}
