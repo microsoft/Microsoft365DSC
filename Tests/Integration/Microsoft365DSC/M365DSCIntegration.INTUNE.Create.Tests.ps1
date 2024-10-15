43ce1606c145080eb4277044950f7fa13f4def3c
NikCharlebois <nicharl@microsoft.com>
2024-10-15 12:34:37 +0000

    Updated {Create} Intune Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.INTUNE.Create.Tests.ps1
@@ -91,12 +91,12 @@
                 IntuneAccountProtectionPolicyWindows10 'myAccountProtectionPolicy'
                 {
                     DisplayName           = 'test'
-                    DeviceSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogDeviceSettings
+                    DeviceSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogDeviceSettings_IntuneAccountProtectionPolicyWindows10
                     {
                         History = 10
                         EnablePinRecovery = 'true'
                     }
-                    UserSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogUserSettings
+                    UserSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogUserSettings_IntuneAccountProtectionPolicyWindows10
                     {
                         History = 20
                         EnablePinRecovery = 'true'
@@ -2627,6 +2627,38 @@
                     notallowedResourceActions = @()
                     roleScopeTagIds           = @('0', '1')
                     Ensure                    = 'Present'
+                    ApplicationId         = $ApplicationId;
+                    TenantId              = $TenantId;
+                    CertificateThumbprint = $CertificateThumbprint;
+                }
+                IntuneSecurityBaselineMicrosoft365AppsForEnterprise 'mySecurityBaselineMicrosoft365AppsForEnterprisePolicy'
+                {
+                    DisplayName           = 'test'
+                    DeviceSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogDeviceSettings_IntuneSecurityBaselineMicrosoft365AppsForEnterprise
+                    {
+                        L_ProtectionFromZoneElevation = '1'
+                        L_grooveexe98 = '1'
+                        L_excelexe99 = '1'
+                        L_mspubexe100 = '1'
+                        L_powerpntexe101 = '1'
+                        L_pptviewexe102 = '1'
+                        L_visioexe103 = '1'
+                        L_winprojexe104 = '1'
+                        L_winwordexe105 = '1'
+                        L_outlookexe106 = '1'
+                        L_spdesignexe107 = '1'
+                        L_exprwdexe108 = '1'
+                        L_msaccessexe109 = '1'
+                        L_onenoteexe110 = '1'
+                        L_mse7exe111 = '1'
+                    }
+                    UserSettings = MSFT_MicrosoftGraphIntuneSettingsCatalogUserSettings_IntuneSecurityBaselineMicrosoft365AppsForEnterprise
+                    {
+                        MicrosoftPublisherV3_Security_TrustCenter_L_BlockMacroExecutionFromInternet = '1'
+                        MicrosoftVisio_Security_TrustCenter_L_VBAWarningsPolicy = '1'
+                        MicrosoftVisio_Security_TrustCenter_L_VBAWarningsPolicy_L_Empty = '3'
+                    }
+                    Ensure                = 'Present'
                     ApplicationId         = $ApplicationId;
                     TenantId              = $TenantId;
                     CertificateThumbprint = $CertificateThumbprint;
