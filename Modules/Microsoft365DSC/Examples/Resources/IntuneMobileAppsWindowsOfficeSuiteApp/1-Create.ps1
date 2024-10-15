3fb01c6d2d43c5e0f31fd5ba47dd06387cae2e77
Katherine Hsu (from Dev Box) <weitzuhsu@microsoft.com>
2024-10-14 17:59:10 -0700

    update create ps1

--- Modules/Microsoft365DSC/Examples/Resources/IntuneMobileAppsWindowsOfficeSuiteApp/1-Create.ps1
+++ Modules/Microsoft365DSC/Examples/Resources/IntuneMobileAppsWindowsOfficeSuiteApp/1-Create.ps1
@@ -19,44 +19,44 @@ Configuration Example
         $CertificateThumbprint
     )
 
-    Import-DscResource -ModuleName Microsoft365DSC
+    # Import-DscResource -ModuleName Microsoft365DSC
     node localhost
     {
-        IntuneMobileAppsWindowsOfficeSuiteApp "IntuneMobileAppsWindowsOfficeSuiteApp-TeamsForBusinessInstaller"
-        {
-            Id                    = "8d027f94-0682-431e-97c1-827d1879fa79";
-            Description           = "TeamsForBusinessInstaller";
-            Developer             = "Contoso";
-            DisplayName           = "TeamsForBusinessInstaller";
-            Ensure                = "Present";
-            InformationUrl        = "";
-            IsFeatured            = $False;
-            MinimumSupportedOperatingSystem = MSFT_DeviceManagementMinimumOperatingSystem{
-                v11_0 = $true
-            }
-            Notes                 = "";
-            Owner                 = "";
-            PrivacyInformationUrl = "";
-            Publisher             = "Contoso";
-            Assignments          = @(
-                    MSFT_DeviceManagementMobileAppAssignment{
-                        groupDisplayName = 'All devices'
-                        deviceAndAppManagementAssignmentFilterType = 'none'
-                        dataType = '#microsoft.graph.allDevicesAssignmentTarget'
-                        intent = 'required'
-                    }
-                    MSFT_DeviceManagementMobileAppAssignment{
-                        deviceAndAppManagementAssignmentFilterType = 'none'
-                        dataType = '#microsoft.graph.groupAssignmentTarget'
-                        groupId = '57b5e81c-85bb-4644-a4fd-33b03e451c89'
-                        intent = 'required'
-                    }
-                );
-            Categories           = @(
-                MSFT_DeviceManagementMobileAppCategory {
-                    Id  = '1bff2652-03ec-4a48-941c-152e93736515'
-                    DisplayName = 'Kajal 3'
-                });
-        }
+        # IntuneMobileAppsWindowsOfficeSuiteApp "IntuneMobileAppsWindowsOfficeSuiteApp-TeamsForBusinessInstaller"
+        # {
+        #     Id                    = "8d027f94-0682-431e-97c1-827d1879fa79";
+        #     Description           = "TeamsForBusinessInstaller";
+        #     Developer             = "Contoso";
+        #     DisplayName           = "TeamsForBusinessInstaller";
+        #     Ensure                = "Present";
+        #     InformationUrl        = "";
+        #     IsFeatured            = $False;
+        #     MinimumSupportedOperatingSystem = MSFT_DeviceManagementMinimumOperatingSystem{
+        #         v11_0 = $true
+        #     }
+        #     Notes                 = "";
+        #     Owner                 = "";
+        #     PrivacyInformationUrl = "";
+        #     Publisher             = "Contoso";
+        #     Assignments          = @(
+        #             MSFT_DeviceManagementMobileAppAssignment{
+        #                 groupDisplayName = 'All devices'
+        #                 deviceAndAppManagementAssignmentFilterType = 'none'
+        #                 dataType = '#microsoft.graph.allDevicesAssignmentTarget'
+        #                 intent = 'required'
+        #             }
+        #             MSFT_DeviceManagementMobileAppAssignment{
+        #                 deviceAndAppManagementAssignmentFilterType = 'none'
+        #                 dataType = '#microsoft.graph.groupAssignmentTarget'
+        #                 groupId = '57b5e81c-85bb-4644-a4fd-33b03e451c89'
+        #                 intent = 'required'
+        #             }
+        #         );
+        #     Categories           = @(
+        #         MSFT_DeviceManagementMobileAppCategory {
+        #             Id  = '1bff2652-03ec-4a48-941c-152e93736515'
+        #             DisplayName = 'Kajal 3'
+        #         });
+        # }
     }
 }
