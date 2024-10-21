31e9aa586b430578d4434bfce5d0d551bbd0db47
NikCharlebois <nicharl@microsoft.com>
2024-10-21 13:22:53 +0000

    Updated {Update} AAD Integration Tests

--- Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Update.Tests.ps1
+++ Tests/Integration/Microsoft365DSC/M365DSCIntegration.AAD.Update.Tests.ps1
@@ -861,6 +861,103 @@
                     TenantId              = $TenantId
                     CertificateThumbprint = $CertificateThumbprint
                 }
+                AADIdentityB2XUserFlow 'AADIdentityB2XUserFlow-B2X_1_TestFlow'
+                {
+                    ApplicationId             = $ApplicationId
+                    TenantId                  = $TenantId
+                    CertificateThumbprint     = $CertificateThumbprint
+                    ApiConnectorConfiguration = MSFT_MicrosoftGraphuserFlowApiConnectorConfiguration
+                    {
+                        postAttributeCollectionConnectorId = 'RestApi_f6e8e73d-6b17-433e-948f-f578f12bd57c'
+                        postFederationSignupConnectorId = 'RestApi_beeb7152-673c-48b3-b143-9975949a93ca'
+                    };
+                    Credential                = $Credscredential;
+                    Ensure                    = "Present";
+                    Id                        = "B2X_1_TestFlow";
+                    IdentityProviders         = @("MSASignup-OAUTH","EmailOtpSignup-OAUTH");
+                    UserAttributeAssignments  = @(
+                        MSFT_MicrosoftGraphuserFlowUserAttributeAssignment
+                        {
+                            UserInputType = 'textBox'
+                            IsOptional = $True
+                            DisplayName = 'Email Address'
+                            Id = 'emailReadonly'
+        
+                        }
+                        MSFT_MicrosoftGraphuserFlowUserAttributeAssignment
+                        {
+                            UserInputType = 'dropdownSingleSelect'
+                            IsOptional = $True
+                            DisplayName = 'Random'
+                            Id = 'city'
+                            UserAttributeValues = @(
+                                MSFT_MicrosoftGraphuserFlowUserAttributeAssignmentUserAttributeValues
+                                {
+                                    IsDefault = $True
+                                    Name = 'S'
+                                    Value = '2'
+                                }
+                                MSFT_MicrosoftGraphuserFlowUserAttributeAssignmentUserAttributeValues
+                                {
+                                    IsDefault = $True
+                                    Name = 'X'
+                                    Value = '1'
+                                }
+                            )
+                        }
+                        MSFT_MicrosoftGraphuserFlowUserAttributeAssignment{
+                            UserInputType = 'textBox'
+                            IsOptional = $False
+                            DisplayName = 'Piyush1'
+                            Id = 'extension_91d51274096941f786b07b9d723d93f4_Piyush1'
+        
+                        }
+                    );
+                }
+                AADIdentityGovernanceLifecycleWorkflow 'AADIdentityGovernanceLifecycleWorkflow-Onboard pre-hire employee updated version'
+                {
+                    Category             = "joiner";
+                    #updated description
+                    Description          = "Updated description the onboard of prehire employee";
+                    DisplayName          = "Onboard pre-hire employee updated version";
+                    Ensure               = "Present";
+                    ExecutionConditions  = MSFT_IdentityGovernanceWorkflowExecutionConditions {
+                        ScopeValue = MSFT_IdentityGovernanceScope {
+                            #updated rule
+                            Rule = '(not (country eq ''America''))'
+                            ODataType = '#microsoft.graph.identityGovernance.ruleBasedSubjectSet'
+                        }
+                        TriggerValue = MSFT_IdentityGovernanceTrigger {
+                            OffsetInDays = 4
+                            TimeBasedAttribute = 'employeeHireDate'
+                            ODataType = '#microsoft.graph.identityGovernance.timeBasedAttributeTrigger'
+                        }
+                        ODataType = '#microsoft.graph.identityGovernance.triggerAndScopeBasedConditions'
+                    };
+                    IsEnabled            = $True;
+                    IsSchedulingEnabled  = $False;
+                    Tasks                = @(
+                        MSFT_AADIdentityGovernanceTask {
+                            DisplayName       = 'Add user to groups'
+                            #updated description
+                            Description       = 'Add user to selected groups updated'
+                            Category          = 'joiner,leaver,mover'
+                            IsEnabled         = $True
+                            ExecutionSequence = 1
+                            ContinueOnError   = $True
+                            TaskDefinitionId   = '22085229-5809-45e8-97fd-270d28d66910'
+                            Arguments         = @(
+                                MSFT_AADIdentityGovernanceTaskArguments {
+                                    Name  = 'groupID'
+                                    Value = '7ad01e00-8c3a-42a6-baaf-39f2390b2565'
+                                }
+                            )
+                        }
+                    );
+                    ApplicationId         = $ApplicationId
+                    TenantId              = $TenantId
+                    CertificateThumbprint = $CertificateThumbprint
+                }
                 AADLifecycleWorkflowSettings 'AADLifecycleWorkflowSettings'
                 {
                     ApplicationId                   = $ApplicationId;
