8ee90d4e3900c87632eefe92a522f79591b88c36
Arpita Mohapatra (from Dev Box) <armohapatra@microsoft.com>
2024-10-22 20:08:14 +0530

    initial changes for ServicePrincipal

---
+++ Modules/Microsoft365DSC/Examples/Resources/EXOServicePrincipal/3-Remove.ps1
@@ -0,0 +1,34 @@
+<#
+This example is used to test new resources and showcase the usage of new resources being worked on.
+It is not meant to use as a production baseline.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost
+    {
+        EXOServicePrincipal 'ServicePrincipal'
+        {
+            AppId                = "703005d9-c467-413e-a085-295c3e09e6cb";
+            Credential           = $Credscredential;
+            DisplayName          = "Aditya Mukund";
+            Ensure               = "Absent";
+            Identity             = "6dfb8885-0297-42e6-9c81-7bf7ee15551d";
+        }
+    }
+}
