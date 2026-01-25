6a0d572e758360b6b233aa18cf9646a4dc57f10a
Mike Poulson <mike@mikepoulson.com>
2026-01-25 15:34:40 -0800

    Initial Add IntuneCorporateDeviceIdentifier

---
+++ Examples/Resources/IntuneCorporateDeviceIdentifier/3-Remove.ps1
@@ -0,0 +1,33 @@
+<#
+This example removes all corporate device identifiers from Intune.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost
+    {
+        IntuneCorporateDeviceIdentifier 'CorporateDevices'
+        {
+            IsSingleInstance      = 'Yes'
+            Ensure                = 'Absent'
+            ApplicationId         = $ApplicationId
+            TenantId              = $TenantId
+            CertificateThumbprint = $CertificateThumbprint
+        }
+    }
+}
